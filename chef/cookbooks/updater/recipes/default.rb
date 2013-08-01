ee5fc6e02d62e5581af30ef5dd6c1d0c53d40abf
Vincent Untz <vuntz@suse.com>
2013-08-01 12:05:14 +0200

    Fix recipe to use correct attribute to know when to reboot
    We were simply never rebooting.

--- chef/cookbooks/updater/recipes/default.rb
+++ chef/cookbooks/updater/recipes/default.rb
@@ -58,7 +58,7 @@ if !node[:updater].has_key?(:one_shot_run) || !node[:updater][:one_shot_run]
             break
           when 102
             # ZYPPER_EXIT_INF_REBOOT_NEEDED
-            if node[:updater][:zypper][:do_reboot]
+            if node[:updater][:do_reboot]
               Chef::Log.info("Will reboot node at the end of chef run.")
               node.run_state[:reboot] = true
               node[:updater][:need_reboot] = false
@@ -91,7 +91,7 @@ if !node[:updater].has_key?(:one_shot_run) || !node[:updater][:one_shot_run]
   end # case
 
   # handle case where there is a reboot needed from a previous run
-  if node[:updater][:zypper][:do_reboot] and node[:updater][:need_reboot]
+  if node[:updater][:do_reboot] and node[:updater][:need_reboot]
     # only reboot if there was no boot since that time
     if node[:uptime_seconds] > Time.now.to_i - node[:updater][:need_reboot_time]
       Chef::Log.info("Will reboot node at the end of chef run.")
