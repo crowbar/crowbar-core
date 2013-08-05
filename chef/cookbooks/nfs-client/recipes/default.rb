53d87060e173f469437133210420caf1078679dc
Vincent Untz <vuntz@suse.com>
2013-08-05 09:23:37 +0200

    Rework everything, except webui which is broken
    Changes:
      - allow defining multiple mounts per proposal
      - forbid nodes to be part of multiple NFS client proposals
      - remove mounts when not in proposal anymore
      - update mounts when options change
      - do not interfere with mounts not managed by barclamp
      - lots of checks

--- chef/cookbooks/nfs-client/recipes/default.rb
+++ chef/cookbooks/nfs-client/recipes/default.rb
@@ -14,60 +14,160 @@
 #
 
 
-#FIXME:
-#  - changing options and re-applying doesn't work?
-#  - changing mount path... does it kill the old one?
-#  - if mount path used by several mounts: what to do?
-#  - how to use more than one proposal on one node? (by default, the json will be overwritten)
+nfs_mounts = {}
+mount_paths = {}
+need_remount = []
 
+comment_option = 'comment="managed-by-crowbar-barclamp-nfs-client"'
 
-mount_path = node[:nfs_client][:mount_path]
-nfs_server = node[:nfs_client][:nfs_server]
-export = node[:nfs_client][:export]
-options = node[:nfs_client][:mount_options]
+### Prepare data about NFS mounts we'll handle
 
-if options.nil?
-  options = []
-end
+node[:nfs_client][:exports].each do |name, data|
+  mount_path = data[:mount_path]
+  nfs_server = data[:nfs_server]
+  export = data[:export]
+  raw_options = data[:mount_options]
 
-# Force nofail option
-if ! options.include?("nofail")
-  nofail_found = false
-
-  options.each do |option|
-    split_options = option.split(',')
-    if split_options.include?("nofail")
-      nofail_found = true
-      break
-    end
+  nfs_mount = "#{nfs_server}:#{export}"
+  if raw_options.nil?
+    raw_options = []
   end
 
-  if ! nofail_found
+  ## Rework options
+  options = []
+  raw_options.each do |option|
+    split_options = option.split(',')
+    options.concat(split_options)
+  end
+
+  # Force nofail option
+  unless options.include?("nofail")
     options << "nofail"
   end
+  # Add comment option (and remove other comment options)
+  options = options.select { |option| not option.start_with?("comment=") }
+  options << comment_option
+
+  ## Some checks
+  if nfs_mounts.has_key?(nfs_mount)
+    raise "NFS mount \"#{nfs_mount}\" is defined multiple times."
+  end
+  if mount_paths.has_key?(mount_path)
+    raise "Mount path \"#{mount_path}\" is used by several NFS mounts."
+  end
+
+  nfs_mounts[nfs_mount] = [mount_path, options]
+  mount_paths[mount_path] = nfs_mount
 end
 
-# Check if mount path can be used
-if ! File.directory?(mount_path)
-  if File.exists?(mount_path)
-    raise "Mount path \"#{mount_path}\" already exists, but is not a directory!"
-  else
-    directory mount_path do
-      owner 'root'
-      group 'root'
-      mode 0755
-      action :create
-      recursive true
+
+### Check the existing state
+
+# Ideally, we'd use something from chef, but it seems there's nothing to list
+# existing mounts. Therefore, we use code inspired from
+# https://github.com/opscode/chef/blob/10-stable/chef/lib/chef/provider/mount/mount.rb
+
+::File.foreach("/etc/fstab") do |line|
+  case line
+  when /^[#\s]/
+    next
+  when /^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/
+    device = $1
+    mount_path = $2
+    fstype = $3
+    options = $4
+
+    owned_by_barclamp = false
+    if options.start_with?(comment_option) or options.include?(',' + comment_option)
+      owned_by_barclamp = true
+    end
+
+    unless owned_by_barclamp
+      if nfs_mounts.has_key?(device)
+        raise "NFS mount \"#{device}\" is already defined in /etc/fstab."
+      elsif mount_paths.has_key?(mount_path)
+        raise "Mount path \"#{mount_path}\" is already used in /etc/fstab."
+      end
+
+      next
+    end
+
+    ## here, we have: owned_by_barclamp == true
+
+    # remove old mount that are not valid anymore
+    remove_mount = false
+
+    if not nfs_mounts.has_key?(device)
+      Chef::Log.info("Removing mount that is not configured anymore: #{device} -> #{mount_path}")
+      remove_mount = true
+    elsif nfs_mounts[device][0] != mount_path
+      Chef::Log.info("Removing mount that does not have correct mount path: #{device} -> #{mount_path}")
+      remove_mount = true
+    elsif mount_paths.has_key?(mount_path) && mount_paths[mount_path] != device
+      # technically, this should actually never be reached, but let's keep this
+      # to be safe
+      Chef::Log.info("Removing mount that does not have correct NFS mount: #{device} -> #{mount_path}")
+      remove_mount = true
+    end
+
+    if remove_mount
+      mount "Removing mount: #{device} -> #{mount_path}" do
+        mount_point mount_path
+        device device
+        action [:disable, :umount]
+      end
+
+      next
+    end
+
+    ## now we only have mounts that are still part of our configuration
+
+    # see if we need to remount instead of mount
+    if fstype != "nfs" || nfs_mounts[device][1].join(',') != options
+      need_remount << device
     end
   end
 end
 
-# Add the mount
-mount mount_path do
-  device "#{nfs_server}:#{export}"
-  fstype "nfs"
-  options options
-  dump 0
-  pass 0
-  action [:mount, :enable]
+
+### Do the real work
+
+nfs_mounts.each do |nfs_mount, data|
+  mount_path, options = data
+
+  # Check if mount path can be used
+  unless File.directory?(mount_path)
+    if File.exists?(mount_path)
+      raise "Mount path \"#{mount_path}\" already exists, but is not a directory!"
+    else
+      directory mount_path do
+        owner 'root'
+        group 'root'
+        mode 0755
+        action :create
+        recursive true
+      end
+    end
+  end
+
+  if need_remount.include?(nfs_mount)
+    # NFS doesn't support the remount option, so manual umount :/
+    # Also we need to disable/reenable to save options because
+    # Chef::Provider::Mount doesn't resave if only options are different.
+    mount "Temporarily removing mount before configuring back due to changed options: #{nfs_mount} -> #{mount_path}" do
+      mount_point mount_path
+      device nfs_mount
+      action [:umount, :disable]
+    end
+  end
+
+  mount "Configuring mount: #{nfs_mount} -> #{mount_path}" do
+    mount_point mount_path
+    device nfs_mount
+    fstype "nfs"
+    options options
+    dump 0
+    pass 0
+    action [:mount, :enable]
+  end
 end
--- crowbar_framework/app/models/nfs_client_service.rb
+++ crowbar_framework/app/models/nfs_client_service.rb
@@ -31,5 +31,53 @@ class NfsClientService < ServiceObject
     true
   end
 
-end
+  def validate_proposal_after_save proposal
+    super
 
+    errors = []
+
+    ### Do not allow multiple mounts with same mount path or same NFS export
+
+    nfs_mounts = {}
+    mount_paths = {}
+
+    proposal["attributes"]["nfs_client"]["exports"].each do |name, data|
+      mount_path = data["mount_path"]
+      nfs_server = data["nfs_server"]
+      export = data["export"]
+      nfs_mount = "#{nfs_server}:#{export}"
+
+      if nfs_mounts.has_key?(nfs_mount)
+        error = "NFS mount \"#{nfs_mount}\" is defined multiple times."
+        errors << error unless errors.include?(error)
+      end
+      if mount_paths.has_key?(mount_path)
+        error = "Mount path \"#{mount_path}\" is used by several NFS mounts."
+        errors << error unless errors.include?(error)
+      end
+
+      nfs_mounts[nfs_mount] = [mount_path]
+      mount_paths[mount_path] = nfs_mount
+    end
+
+    ### Do not allow elements of this proposal to be in another proposal, since
+    ### the configuration cannot be shared.
+    elements = proposal["deployment"]["nfs_client"]["elements"]["nfs-client"]
+
+    proposals_raw.each do |p|
+      next if p["id"] == proposal["id"]
+
+      p["deployment"]["nfs_client"]["elements"]["nfs-client"].each do |e|
+        if elements.include?(e)
+          p_name = p["id"].gsub("bc-#{@bc_name}-", "")
+          errors << "Nodes cannot be part of multiple NFS client proposals, but #{e} is already part of proposal \"#{p_name}\"."
+        end
+      end
+    end
+
+    if errors.length > 0
+      raise Chef::Exceptions::ValidationFailed.new(errors.join("\n"))
+    end
+  end
+
+end
