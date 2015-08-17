68c31b04c090d67089549aa108bd3a7cfccbac3b
Maximilian Meister <mmeister@suse.de>
2015-08-17 16:44:55 +0200

    Revert "apply_role: disable 15-minute chef run during apply_role"
    This reverts commit d7a3ac0a628c66122e98632c23fea435e506eb87.

--- crowbar_framework/app/models/service_object.rb
+++ crowbar_framework/app/models/service_object.rb
@@ -822,9 +822,6 @@ class ServiceObject
     # We also check that all nodes we'll require are in the ready state.
     #
 
-    # Initialize variables used in ensure at the end of the method
-    chef_daemon_nodes = []
-
     # Query for this role
     old_role = RoleObject.find_role_by_name(role.name)
 
@@ -855,10 +852,6 @@ class ServiceObject
       end
     end
     new_elements = expanded_new_elements
-
-    # stop chef daemon on all nodes
-    chef_daemon_nodes = new_elements.values.flatten.uniq
-    chef_daemon(:stop, chef_daemon_nodes)
 
     # save list of expanded elements, as this is needed when we look at the old
     # role. See below the comments for old_elements.
@@ -1284,9 +1277,6 @@ class ServiceObject
     restore_to_ready(applying_nodes)
     process_queue unless in_queue
     [200, {}]
-  ensure
-    # start chef daemon on all nodes
-    chef_daemon(:start, chef_daemon_nodes)
   end
 
   def apply_role_pre_chef_call(old_role, role, all_nodes)
@@ -1410,26 +1400,6 @@ class ServiceObject
       # check if we need to wait for a node reboot
       wait_for_reboot(node)
     }
-  end
-
-  def chef_daemon(action, node_list)
-    wait_nodes = []
-
-    node_list.each do |node_name|
-      node = NodeObject.find_node_by_name(node_name)
-
-      # we can't connect to windows nodes
-      next if node[:platform] == "windows"
-
-      @logger.debug "apply_role: #{action.to_s} chef service on #{node_name}"
-      node.run_service :chef, action
-      wait_nodes << node_name
-    end
-
-    # wait for chef clients on all nodes
-    wait_nodes.each do |node_name|
-      wait_for_chef_clients(node_name, logger: true)
-    end if action == :stop
   end
 
   private
