a321d4cda9f14685cf914185e091ff2cb3bdc911
Adam Spiers <aspiers@suse.com>
2015-09-07 01:31:42 +0100

    split ssh code out from provisioner::base recipe
    provisioner::base is already horrendously overgrown, so let's start
    splitting chunks off into separate recipes.

--- chef/cookbooks/provisioner/recipes/base.rb
+++ chef/cookbooks/provisioner/recipes/base.rb
@@ -26,13 +26,6 @@ package "ipmitool" do
   action :install
 end
 
-directory "/root/.ssh" do
-  owner "root"
-  group "root"
-  mode "0700"
-  action :create
-end
-
 # We don't want to use bluepill on SUSE and Windows
 unless node[:platform_family] == "suse"
   # Make sure we have Bluepill
@@ -42,37 +35,7 @@ unless node[:platform_family] == "suse"
   end
 end
 
-node.set["crowbar"]["ssh"] ||= {}
-
-# Start with a blank slate, to ensure that any keys removed from a
-# previously applied proposal will be removed.  It also means that any
-# keys manually added to authorized_keys will be automatically removed
-# by Chef.
-node.set["crowbar"]["ssh"]["access_keys"] = {}
-
-# Build my key
-if ::File.exists?("/root/.ssh/id_rsa.pub") == false
-  %x{ssh-keygen -t rsa -f /root/.ssh/id_rsa -N ""}
-end
-
-str = %x{cat /root/.ssh/id_rsa.pub}.chomp
-node.set["crowbar"]["ssh"]["root_pub_key"] = str
-node.set["crowbar"]["ssh"]["access_keys"][node.name] = str
-
-# Add additional keys
-node["provisioner"]["access_keys"].strip.split("\n").each do |key|
-  key.strip!
-  if !key.empty?
-    nodename = key.split(" ")[2]
-    node.set["crowbar"]["ssh"]["access_keys"][nodename] = key
-  end
-end
-
-pkey = provisioner_server_node["crowbar"]["ssh"]["root_pub_key"] rescue nil
-ps_name = provisioner_server_node.name
-if !pkey.nil? and pkey != node["crowbar"]["ssh"]["access_keys"][ps_name]
-  node.set["crowbar"]["ssh"]["access_keys"][ps_name] = pkey
-end
+include_recipe "provisioner::ssh"
 
 # Fix bug we had in stoney and earlier where we never saved the target_platform
 # of the node when the node was installed with the default target platform.
@@ -84,38 +47,11 @@ end
 
 node.save
 
-template "/root/.ssh/authorized_keys" do
-  owner "root"
-  group "root"
-  mode "0644"
-  action :create
-  source "authorized_keys.erb"
-  variables(keys: node["crowbar"]["ssh"]["access_keys"])
-end
-
 template "/etc/sudo.conf" do
   source "sudo.conf.erb"
   owner "root"
   group "root"
   mode "0644"
-end
-
-# Also put authorized_keys in tftpboot path on the admin node so that discovered
-# nodes can use the same.
-if node.roles.include? "crowbar"
-  template "#{node[:provisioner][:root]}/authorized_keys" do
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
-    source "authorized_keys.erb"
-    variables(keys: node["crowbar"]["ssh"]["access_keys"])
-  end
-end
-
-bash "Disable Strict Host Key checking" do
-  code "echo '    StrictHostKeyChecking no' >>/etc/ssh/ssh_config"
-  not_if "grep -q 'StrictHostKeyChecking no' /etc/ssh/ssh_config"
 end
 
 bash "Set EDITOR=vi environment variable" do
