d23fc78926bd07071a9d5bac31d63059926d1326
Ralf Haferkamp <rhafer@suse.de>
2015-09-30 19:35:22 +0200

    Update node cache when setting node state to applying
    set_to_applying() calls save() on the node, so it should be called on the node
    object from the cache instead of loading it from the database. Otherwise the
    cached object will be outdated and we run into a revision race later.

--- crowbar_framework/app/models/service_object.rb
+++ crowbar_framework/app/models/service_object.rb
@@ -160,10 +160,13 @@ class ServiceObject
 # Helper routines for queuing
 #
 
-  def set_to_applying(nodes, inst)
+  def set_to_applying(nodes, inst, pre_cached_nodes)
     with_lock "BA-LOCK" do
       nodes.each do |node_name|
-        node = NodeObject.find_node_by_name(node_name)
+        node = pre_cached_nodes[node_name]
+        if node.nil?
+          node = NodeObject.find_node_by_name(node_name)
+        end
         next if node.nil?
 
         node.crowbar["state"] = "applying"
@@ -1079,7 +1082,7 @@ class ServiceObject
 
     # Mark nodes as applying; beware that all_nodes do not contain nodes that
     # are actually removed.
-    set_to_applying(applying_nodes, inst)
+    set_to_applying(applying_nodes, inst, pre_cached_nodes)
 
     # Part III: Update run lists of nodes to reflect new deployment. I.e. write
     # through the deployment schedule in pending node actions into run lists.
