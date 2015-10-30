d033be9aae060c0d0a4a448eef6b4344886bd695
Vincent Untz <vuntz@suse.com>
2015-10-30 16:10:01 +0100

    bind9: clear nscd cache after dns config change

--- chef/cookbooks/bind9/recipes/default.rb
+++ chef/cookbooks/bind9/recipes/default.rb
@@ -369,5 +369,11 @@ service "bind9" do
   action [:enable, :start]
 end
 
+execute "reload nscd after dns config change" do
+  command "nscd -i hosts"
+  action :nothing
+  subscribes :run, "template[/etc/bind/db.#{node[:dns][:domain]}]"
+end
+
 node.set[:dns][:zones]=zones
 include_recipe "resolver"
