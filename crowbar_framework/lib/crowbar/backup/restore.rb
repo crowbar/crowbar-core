crowbar/crowbar-core

commit 1923f99783fef3c53705da632821b9f89bc0ba26

Fix restoring directories
When restoring a directory we only want the contents of the directory be
restored and not the directory itself, otherwise we get e.g. ".ssh" restored
into ".ssh/.ssh".

crowbar_framework/lib/crowbar/backup/restore.rb
@@ -66,10 +66,20 @@ module Crowbar
       def restore_crowbar
         Crowbar::Backup::Base.restore_files.each do |source, destination|
           # keep the permissions of the files that are already in place
+          src_path = @data.join("crowbar", source)
+          dest_path = Pathname.new(destination)
+          # If source and destination are both directories we just need to
+          # copy the contents of source, not the directory itself.
+          src_string = if dest_path.directory? && src_path.directory?
+            "#{srcpath}/."
+          else
+            srcpath.to_s
+          end
+
           system(
             "sudo", "-i",
             "cp", "-r", "--no-preserve=mode,ownership",
-            @data.join("crowbar", source).to_s,
+            src_string,
             destination
           )
         end

commit 8e8cce2bd4408defd68c898b7d559952d0b52f44

Fix directory restore once more
Commit 1923f997 introduced a typo (src_path vs srcpath). Also we need to check
the destination directory as root since it might be root owned and not visible
to the "crowbar" user.

crowbar_framework/lib/crowbar/backup/restore.rb
@@ -67,13 +67,14 @@ module Crowbar
         Crowbar::Backup::Base.restore_files.each do |source, destination|
           # keep the permissions of the files that are already in place
           src_path = @data.join("crowbar", source)
-          dest_path = Pathname.new(destination)
+          dest_is_dir = system("sudo", "-i", "test", "-d", destination)
+
           # If source and destination are both directories we just need to
           # copy the contents of source, not the directory itself.
-          src_string = if dest_path.directory? && src_path.directory?
-            "#{srcpath}/."
+          src_string = if dest_is_dir && src_path.directory?
+            "#{src_path}/."
           else
-            srcpath.to_s
+            src_path.to_s
           end
 
           system(