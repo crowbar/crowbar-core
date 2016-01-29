449f58c1c7453a27c8067eafdb0799c38c17a9e8
Jiří Suchomel <jsuchome@suse.cz>
2016-01-29 16:20:03 +0100

    Use only one role for upgrade related tasks
    and decide what to do based on the value of crowbar_upgrade_step.

--- chef/cookbooks/crowbar/recipes/crowbar-upgrade.rb
+++ chef/cookbooks/crowbar/recipes/crowbar-upgrade.rb
@@ -44,6 +44,83 @@ when "openstack_shutdown"
       done
     EOF
   end
+
+when "dump_openstack_database"
+
+  include_recipe "crowbar::stop-services-before-upgrade"
+
+  # If postgres is not running here, it means we're in the DB cluster and database runs
+  # on the other node: let the other node take care of the rest.
+  unless ::Kernel.system("service postgresql status")
+    if node[:crowbar][:upgrade][:db_dumped_here]
+      node[:crowbar][:upgrade].delete :db_dumped_here
+      node.save
+    end
+    return
+  end
+
+  # Check the available space before the dump
+
+  dump_location = node[:crowbar][:upgrade][:db_dump_location]
+
+  ruby_block "check available space" do
+    block do
+      require "pathname"
+      path = Pathname.new(dump_location).split.first.to_s
+      available = `df #{path} | tail -n 1 | sed "s/ \\+/ /g" | cut -d " " -f 4`
+      db_size = `su - postgres -c 'pg_dumpall | wc -c'`
+      if db_size.to_i > available.to_i
+        message = "Not enough space for the Database dump on node #{node.name}!\n" \
+          "Database size is: #{db_size}B, available space on #{path} is only #{available}B."
+        Chef::Log.fatal(message)
+        node["crowbar_wall"]["chef_error"] = message
+        node.save
+        raise message
+      end
+    end
+  end
+
+  Chef::Log.info("dumping DB into #{dump_location}")
+
+  # Dump the database at the DB node
+  bash "dump database content" do
+    code <<-EOF
+      su - postgres -c 'pg_dumpall > #{dump_location}'
+    EOF
+  end
+
+  # we have to indicate the node where the dump is actually located
+  node.set[:crowbar][:upgrade][:db_dumped_here] = true
+  node.save
+
+when "db_shutdown"
+
+  # Stop remaining pacemaker resources
+  bash "stop pacemaker resources" do
+    code <<-EOF
+      for type in clone ms primitive; do
+        for resource in $(crm configure show | grep ^$type | cut -d " " -f2);
+        do
+          crm resource stop $resource
+        done
+      done
+    EOF
+    only_if { ::File.exist?("/usr/sbin/crm") }
+  end
+
+  # Stop the database and corosync
+  bash "stop the database" do
+    code <<-EOF
+      for i in /etc/init.d/drbd \
+               /etc/init.d/openais \
+               /etc/init.d/postgresql;
+      do
+        if test -e $i; then
+          $i stop
+        fi
+      done
+    EOF
+  end
 when "done_openstack_shutdown", "wait_for_openstack_shutdown"
   Chef::Log.debug("Nothing to do on this node, waiting for others to finish their work...")
 else
