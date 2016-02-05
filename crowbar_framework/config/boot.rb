5bac330b51b152242f24c6fb01052ce7fcb083f5
Vincent Untz <vuntz@suse.com>
2016-02-05 08:59:38 +0100

    crowbar: Do not make rails app depend on passenger gem
    There's actually nothing in the rails app depending on it, it's just a
    requirement for the way we deploy the rails app.

--- crowbar_framework/config/boot.rb
+++ crowbar_framework/config/boot.rb
@@ -36,9 +36,6 @@ else
   gem "sass-rails", version: "~> 5.0.3"
   require "sass-rails"
 
-  gem "passenger", version: "~> 5.0.18"
-  require "passenger"
-
   # general stuff
   gem "activerecord-session_store", version: "~> 0.1.0"
   require "activerecord/session_store"
