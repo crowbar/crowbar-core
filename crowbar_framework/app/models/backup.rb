ebd90418f8c6d75fdd1b4b4a2ae276d3b8e5a85c
Maximilian Meister <mmeister@suse.de>
2016-02-18 14:41:10 +0100

    make sure the metadata is valid yaml
    (cherry picked from commit e89261725237d1c48929646a874b9b121467b721)

--- crowbar_framework/app/models/backup.rb
+++ crowbar_framework/app/models/backup.rb
@@ -185,17 +185,24 @@ class Backup < ActiveRecord::Base
       return false
     end
 
-    if data.join("meta.yml").exist?
-      meta = YAML.load_file(data.join("meta.yml"))
-      self.version = meta["version"].to_s
-      self.size = path.size
-      self.created_at = Time.zone.parse(meta["created_at"])
-      # 20151222144602_create_backups.rb
-      self.migration_level = meta["migration_level"] || 20151222144602
-    else
+    meta_file = data.join("meta.yml")
+    unless meta_file.exist?
+      errors.add(:file_content, I18n.t("backups.index.meta_missing"))
+      return false
+    end
+
+    begin
+      meta = YAML.load_file(meta_file)
+    rescue Psych::SyntaxError
       errors.add(:file_content, I18n.t("backups.index.invalid_file_content"))
       return false
     end
+
+    self.version = meta["version"].to_s
+    self.size = path.size
+    self.created_at = Time.zone.parse(meta["created_at"])
+    # 20151222144602_create_backups.rb
+    self.migration_level = meta["migration_level"] || 20151222144602
 
     path.open("wb") do |f|
       f.write(file.read)
