6d91977885b78c8f37f7e356c59ca9b2ee2a4785
Maximilian Meister <mmeister@suse.de>
2016-03-15 16:25:37 +0100

    prevent deleting paths outside the rails tmp dir

--- crowbar_framework/app/models/backup.rb
+++ crowbar_framework/app/models/backup.rb
@@ -110,7 +110,7 @@ class Backup < ActiveRecord::Base
   end
 
   def cleanup
-    return unless @data
+    return unless @data || @data.to_s =~ /\A#{Rails.root.join("tmp").to_s}/
 
     system(
       "sudo",
