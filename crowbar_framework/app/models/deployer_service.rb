908ec8903b82694b1baf3fd9a14c7536d5347c47
Vincent Untz <vuntz@suse.com>
2016-06-28 11:59:55 +0200

    crowbar: Cleanup transition in DeployerService

--- crowbar_framework/app/models/deployer_service.rb
+++ crowbar_framework/app/models/deployer_service.rb
@@ -34,15 +34,7 @@ class DeployerService < ServiceObject
   end
 
   def transition(inst, name, state)
-    @logger.debug("Deployer transition: entering #{name} for #{state}")
-
-    save_it = false
-
-    node = NodeObject.find_node_by_name(name)
-    if node.nil?
-      @logger.error("Deployer transition: leaving #{name} for #{state}: Node not found")
-      return [404, "Failed to find node"]
-    end
+    @logger.debug("Deployer transition: entering: #{name} for #{state}")
 
     # discovering because mandatory for discovery image
     if ["discovering", "readying"].include? state
@@ -56,70 +48,34 @@ class DeployerService < ServiceObject
       end
     end
 
-    #
-    # The temp booting images need to have clients cleared.
-    # After installation, there is also no client available
-    #
-    if [
-      "discovering", "discovered",
-      "hardware-installing", "hardware-installed",
-      "hardware-updating", "hardware-updated",
-      "installing", "installed",
-      "delete",
-      "reset", "reinstall",
-      "update"
-    ].member?(state) && !node.admin?
-      @logger.debug("Deployer transition: should be deleting a client entry for #{node.name}")
-      client = ClientObject.find_client_by_name node.name
-      @logger.debug("Deployer transition: found and trying to delete a client entry for #{node.name}") unless client.nil?
-      client.destroy unless client.nil?
-
-      # Make sure that the node can be accessed by knife ssh or ssh
-      if ["reset", "reinstall", "update", "delete"].member?(state)
-        system("sudo rm -f /root/.ssh/known_hosts")
-      end
-    end
-
-    # if delete - clear out stuff
-    if state == "delete"
-      # Do more work here - one day.
-      return [200, { name: name }]
-    end
-
-    #
-    # Decide on the nodes role for the cloud
-    #   * This includes adding a role for node type (for bios/raid update/config)
-    #   * This includes adding an attribute on the node for inclusion in clouds
-    #
     if state == "discovered"
-      @logger.debug("Deployer transition: discovered state for #{name}")
+      node = NodeObject.find_node_by_name(name)
 
       if node.admin?
-      # We are an admin node - display bios updates for now.
+        # We are an admin node - display bios updates for now.
         node.crowbar["bios"] ||= {}
         node.crowbar["bios"]["bios_setup_enable"] = false
         node.crowbar["bios"]["bios_update_enable"] = false
         node.crowbar["raid"] ||= {}
         node.crowbar["raid"]["enable"] = false
-        save_it = true
+        node.save
+      else
+        # do we auto-allocate?
+        role = RoleObject.find_role_by_name "#{@bc_name}-config-#{inst}"
+        unless role.default_attributes["deployer"]["use_allocate"]
+          @logger.debug("Automatically allocating node #{name}")
+          node.allocate!
+        end
       end
-
-      node.save if save_it
-
-      role = RoleObject.find_role_by_name "deployer-config-#{inst}"
-      unless role.default_attributes["deployer"]["use_allocate"] && !node.admin?
-        @logger.debug("Automatically allocating node #{node.name}")
-        node.allocate!
-      end
-
-      @logger.debug("Deployer transition: leaving discovered for #{name} EOF")
-      return [200, { name: name }]
     end
 
+    # Decide on the nodes role for the cloud
+    #   * This includes adding a role for node type (for bios/raid update/config)
+    #   * This includes adding an attribute on the node for inclusion in clouds
     #
-    # Once we have been allocated, we will fly through here and we will setup the raid/bios info
-    #
+    # Once we have been allocated, we setup the raid/bios info
     if state == "hardware-installing"
+      node = NodeObject.find_node_by_name(name)
       # build a list of current and pending roles to check against
       roles = node.roles.dup
       unless node.crowbar["crowbar"]["pending"].nil?
@@ -127,12 +83,12 @@ class DeployerService < ServiceObject
       end
 
       # Walk map to categorize the node.  Choose first one from the bios map that matches.
-      role = RoleObject.find_role_by_name "deployer-config-#{inst}"
+      role = RoleObject.find_role_by_name "#{@bc_name}-config-#{inst}"
       done = false
       role.default_attributes["deployer"]["bios_map"].each do |match|
         roles.each do |r|
           if r =~ /#{match["pattern"]}/
-            node.crowbar["crowbar"]["hardware"] = {} if node.crowbar["crowbar"]["hardware"].nil?
+            node.crowbar["crowbar"]["hardware"] ||= {}
             node.crowbar["crowbar"]["hardware"]["bios_set"] = match["bios_set"] if node.crowbar["crowbar"]["hardware"]["bios_set"].nil?
             node.crowbar["crowbar"]["hardware"]["raid_set"] = match["raid_set"] if node.crowbar["crowbar"]["hardware"]["raid_set"].nil?
             done = true
@@ -144,12 +100,38 @@ class DeployerService < ServiceObject
 
       os_map = role.default_attributes["deployer"]["os_map"]
       node.crowbar["crowbar"]["hardware"]["os"] = os_map[0]["install_os"]
-      save_it = true
+      node.save
     end
 
-    node.save if save_it
+    # The discovery image needs to have clients cleared.
+    # After installation, there is also no client available
+    if [
+      "discovering", "discovered",
+      "hardware-installing", "hardware-installed",
+      "hardware-updating", "hardware-updated",
+      "installing", "installed",
+      "delete",
+      "reset", "reinstall",
+      "update"
+    ].member?(state)
+      node = NodeObject.find_node_by_name(name)
+      client = ClientObject.find_client_by_name name
+      unless node.admin? || client.nil?
+        @logger.debug("Deployer transition: deleting a chef client for #{name}")
+        client.destroy
+      end
+    end
 
-    @logger.debug("Deployer transition: leaving state for #{name} EOF")
+    # Make sure that the node can be accessed by knife ssh or ssh
+    if ["reset", "reinstall", "update", "delete"].member?(state)
+      system("sudo rm -f /root/.ssh/known_hosts")
+    end
+
+    if state == "delete"
+      # Do more work here - one day.
+    end
+
+    @logger.debug("Deployer transition: exiting: #{name} for #{state}")
     return [200, { name: name }]
   end
 end
