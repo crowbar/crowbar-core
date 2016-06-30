69c5726fc9563d86c9bf82cc3f3f729708d8a3b3
Maximilian Meister <mmeister@suse.de>
2016-06-30 14:04:43 +0200

    fix hound complaints

--- crowbar_framework/app/controllers/barclamp_controller.rb
+++ crowbar_framework/app/controllers/barclamp_controller.rb
@@ -800,18 +800,31 @@ class BarclampController < ApplicationController
     modules = {}
     active = RoleObject.active
     barclamps.each do |name, details|
-      modules[name] = { description: details["description"] || t("not_set"), order: details["order"], proposals: {}, expand: false, members: (details["members"].nil? ? 0 : details["members"].length) }
+      modules[name] = {
+        description: details["description"] || t("not_set"), order: details["order"],
+        proposals: {},
+        expand: false,
+        members: (details["members"].nil? ? 0 : details["members"].length)
+      }
 
       bc_service = ServiceObject.get_service(name)
       modules[name][:allow_multiple_proposals] = bc_service.allow_multiple_proposals?
       suggested_proposal_name = bc_service.suggested_proposal_name
 
       Proposal.where(barclamp: name).each do |prop|
-        # active is ALWAYS true if there is a role and or status maybe true if the status is ready, unready, or pending.
-        status = (["unready", "pending"].include?(prop.status) or active.include?("#{name}_#{prop.name}"))
-        @count += 1 unless @count<0  #allows caller to skip incrementing by initializing to -1
-        modules[name][:proposals][prop.name] = {id: prop.id, description: prop.description, status: (status ? prop.status : "hold"), active: status}
-        if prop.status === "failed"
+        # active is ALWAYS true if there is a role and or status maybe true if the status is
+        # ready, unready or pending.
+        status = (
+          ["unready", "pending"].include?(prop.status) || active.include?("#{name}_#{prop.name}")
+        )
+        @count += 1 unless @count < 0 # allows caller to skip incrementing by initializing to -1
+        modules[name][:proposals][prop.name] = {
+          id: prop.id,
+          description: prop.description,
+          status: (status ? prop.status : "hold"),
+          active: status
+        }
+        if prop.status == "failed"
           modules[name][:proposals][prop.name][:message] = prop.fail_reason
           modules[name][:expand] = true
         end
