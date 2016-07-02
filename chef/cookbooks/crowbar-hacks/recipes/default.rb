7751b2c10541f170fc28d59a8d893ee0bcfff883
Vincent Untz <vuntz@suse.com>
2016-07-02 07:59:12 +0200

    crowbar-hacks: Remove check for node state in default recipe
    This was to avoid running the code in the discovery image. The recipe
    isn't included in that case anymore.

--- chef/cookbooks/crowbar-hacks/recipes/default.rb
+++ chef/cookbooks/crowbar-hacks/recipes/default.rb
@@ -16,62 +16,60 @@
 # This recipe is a placeholder for misc. hacks we want to do on every node,
 # but that do not really belong with any specific barclamp.
 
-states = ["ready", "readying", "recovering", "applying"]
-if states.include?(node[:state])
-  unless %w(suse windows).include?(node[:platform_family])
-    # Don't waste time with mlocate or updatedb
-    %w{mlocate mlocate.cron updatedb}.each do |f|
-      file "/etc/cron.daily/#{f}" do
-        action :delete
-      end
+unless %w(suse windows).include?(node[:platform_family])
+  # Don't waste time with mlocate or updatedb
+  %w{mlocate mlocate.cron updatedb}.each do |f|
+    file "/etc/cron.daily/#{f}" do
+      action :delete
     end
-
-    template "/etc/logrotate.d/chef" do
-      source "logrotate.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(logfiles: "/var/log/chef/client.log",
-                postrotate: "bluepill chef-client restart")
-    end
-
-    # Set up some basic log rotation
-    template "/etc/logrotate.d/crowbar-webserver" do
-      source "logrotate.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(logfiles: "/var/log/crowbar/*.log /var/log/crowbar/*.out /var/log/crowbar/chef-client/*.log",
-                  action: "create 644 crowbar crowbar",
-                postrotate: "/usr/bin/pumactl -S /opt/dell/crowbar_framework/tmp/pids/puma.state phased-restart")
-    end if node[:recipes].include?("crowbar")
-    template "/etc/logrotate.d/node-logs" do
-      source "logrotate.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(logfiles: "/var/log/nodes/*.log /var/log/nodes/.log",
-                postrotate: "/usr/bin/killall -HUP rsyslogd")
-    end if node[:recipes].include?("logging::server")
-    template "/etc/logrotate.d/client-join-logs" do
-      source "logrotate.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(logfiles: ["/var/log/crowbar/crowbar_join/*"])
-    end unless node[:recipes].include?("crowbar")
   end
-  # Note: Hacks that are needed on SUSE platforms as well too come here
 
-  if %w(suse rhel).include?(node[:platform_family])
-    # Workaround sysctl not loading configs from /etc/sysctl.d/
-    # during reboot
-    directory "create /etc/sysctl.d for reload-sysctl.d cronjob" do
-      path "/etc/sysctl.d"
-      mode "755"
-    end
-    cookbook_file "/etc/cron.d/reload-sysctl.d" do
-      source "reload-sysctl-d.cron"
-    end
+  template "/etc/logrotate.d/chef" do
+    source "logrotate.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(logfiles: "/var/log/chef/client.log",
+              postrotate: "bluepill chef-client restart")
+  end
+
+  # Set up some basic log rotation
+  template "/etc/logrotate.d/crowbar-webserver" do
+    source "logrotate.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(logfiles: "/var/log/crowbar/*.log /var/log/crowbar/*.out /var/log/crowbar/chef-client/*.log",
+              action: "create 644 crowbar crowbar",
+              postrotate: "/usr/bin/pumactl -S /opt/dell/crowbar_framework/tmp/pids/puma.state phased-restart")
+  end if node[:recipes].include?("crowbar")
+  template "/etc/logrotate.d/node-logs" do
+    source "logrotate.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(logfiles: "/var/log/nodes/*.log /var/log/nodes/.log",
+              postrotate: "/usr/bin/killall -HUP rsyslogd")
+  end if node[:recipes].include?("logging::server")
+  template "/etc/logrotate.d/client-join-logs" do
+    source "logrotate.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(logfiles: ["/var/log/crowbar/crowbar_join/*"])
+  end unless node[:recipes].include?("crowbar")
+end
+
+# Note: Hacks that are needed on SUSE platforms as well come here
+
+if %w(suse rhel).include?(node[:platform_family])
+  # Workaround sysctl not loading configs from /etc/sysctl.d/
+  # during reboot
+  directory "create /etc/sysctl.d for reload-sysctl.d cronjob" do
+    path "/etc/sysctl.d"
+    mode "755"
+  end
+  cookbook_file "/etc/cron.d/reload-sysctl.d" do
+    source "reload-sysctl-d.cron"
   end
 end
