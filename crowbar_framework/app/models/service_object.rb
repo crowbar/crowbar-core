8774bb25943dec10dc579d7dec9b5d87d1166fac
Vincent Untz <vuntz@suse.com>
2016-07-10 06:44:39 +0200

    crowbar: Improve debug log in ServiceObject.apply_role
    This includes removing log lines which duplicate data already in the
    debug logs.

--- crowbar_framework/app/models/service_object.rb
+++ crowbar_framework/app/models/service_object.rb
@@ -1040,7 +1040,7 @@ class ServiceObject
     element_order.each do |roles|
       # roles is an Array of names of Chef roles which can all be
       # applied in parallel.
-      @logger.debug "elems #{roles.inspect}"
+      @logger.debug "Preparing batch with following roles: #{roles.inspect}"
 
       # A list of nodes changed when applying roles from this batch
       nodes_in_batch = []
@@ -1053,9 +1053,9 @@ class ServiceObject
         old_nodes = old_elements[role_name] || []
         new_nodes = new_elements[role_name] || []
 
-        @logger.debug "role_name #{role_name.inspect}"
-        @logger.debug "old_nodes #{old_nodes.inspect}"
-        @logger.debug "new_nodes #{new_nodes.inspect}"
+        @logger.debug "Preparing role #{role_name} for batch:"
+        @logger.debug "  Nodes in old applied proposal for role: #{old_nodes.inspect}"
+        @logger.debug "  Nodes in new applied proposal for role: #{new_nodes.inspect}"
 
         remove_role_name = "#{role_name}_remove"
 
@@ -1201,7 +1201,7 @@ class ServiceObject
 
     # Part III: Update run lists of nodes to reflect new deployment. I.e. write
     # through the deployment schedule in pending node actions into run lists.
-    @logger.debug "Clean the run_lists for #{pending_node_actions.inspect}"
+    @logger.debug "Update the run_lists for #{pending_node_actions.inspect}"
 
     pending_node_actions.each do |node_name, lists|
       # pre_cached_nodes contains only new_nodes, we need to look up the
@@ -1214,9 +1214,6 @@ class ServiceObject
 
       rlist = lists[:remove]
       alist = lists[:add]
-
-      @logger.debug "rlist #{rlist.pretty_inspect}"
-      @logger.debug "alist #{alist.pretty_inspect}"
 
       # Remove the roles being lost
       rlist.each do |item|
@@ -1238,7 +1235,6 @@ class ServiceObject
         save_it = node.delete_from_run_list(role.name) || save_it
       end
 
-      @logger.debug("AR: Saving node #{node.name}") if save_it
       node.save if save_it
     end
 
