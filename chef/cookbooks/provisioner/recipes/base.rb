818e5aa555f5493efa0174af2b911dc8f81f495c
Maximilian Meister <mmeister@suse.de>
2016-07-11 15:27:44 +0200

    parse the output earlier with map

--- chef/cookbooks/provisioner/recipes/base.rb
+++ chef/cookbooks/provisioner/recipes/base.rb
@@ -306,10 +306,8 @@ if node[:platform_family] == "suse" && !node.roles.include?("provisioner-server"
 
       out = `LANG=C zypper --non-interactive repos #{name} 2> /dev/null`
       out.split("\n").each do |line|
-        attribute, value = line.split(":", 2)
+        attribute, value = line.split(":", 2).map(&:strip)
         next if value.nil?
-        attribute.strip!
-        value.strip!
         if attribute == "URI"
           current_url = value
         elsif attribute == "Priority"
