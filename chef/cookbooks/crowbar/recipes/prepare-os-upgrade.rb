cc27bef434242498833d97a4bd498476b2b5e571
Jiří Suchomel <jiri.suchomel@suse.com>
2016-09-05 11:07:03 +0200

    Replace the base installation repository with a new one

--- chef/cookbooks/crowbar/recipes/prepare-os-upgrade.rb
+++ chef/cookbooks/crowbar/recipes/prepare-os-upgrade.rb
@@ -20,14 +20,38 @@
 # 2. adds correct new ones
 # 3. runs zypper dup to upgrade the node
 
+arch = node[:kernel][:machine]
+
 old_repos = Provisioner::Repositories.get_repos(
-  node[:platform], node[:platform_version], node[:kernel][:machine]
+  node[:platform], node[:platform_version], arch
 )
 
 target_platform, target_platform_version = node[:target_platform].split("-")
 new_repos = Provisioner::Repositories.get_repos(
-  target_platform, target_platform_version, node[:kernel][:machine]
+  target_platform, target_platform_version, arch
 )
+
+# Find out the location of the base system repository
+provisioner = search(:node, "roles:provisioner-server")[0]
+admin_ip = Barclamp::Inventory.get_network_by_type(provisioner, "admin").address
+
+web_port = provisioner[:provisioner][:web_port]
+provisioner_web = "http://#{admin_ip}:#{web_port}"
+
+web_path = "#{provisioner_web}/#{node[:platform]}-#{node[:platform_version]}/#{arch}"
+old_install_url = "#{web_path}/install"
+
+web_path = "#{provisioner_web}/#{node[:target_platform]}/#{arch}"
+new_install_url = "#{web_path}/install"
+
+# try to create an alias for new base repo from the original base repo
+repo_alias = "SLES12-SP1-12.1-0"
+doc = REXML::Document.new(`zypper --xmlout lr --details`)
+doc.elements.each("stream/repo-list/repo") do |repo|
+  repo_alias = repo.attributes["alias"] if repo.elements["url"].text == old_install_url
+end
+
+new_alias = repo_alias.gsub("SP1", "SP2").gsub(node[:platform_version], target_platform_version)
 
 template "/usr/sbin/crowbar-upgrade-os.sh" do
   source "crowbar-upgrade-os.erb"
@@ -38,6 +62,9 @@ template "/usr/sbin/crowbar-upgrade-os.sh" do
   variables(
     old_repos: old_repos,
     new_repos: new_repos,
-    target_platform_version: target_platform_version
+    target_platform_version: target_platform_version,
+    old_base_repo: old_install_url,
+    new_base_repo: new_install_url,
+    new_alias: new_alias
   )
 end
