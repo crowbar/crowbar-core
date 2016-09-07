990196779e2231bdf9a2d53901d5978bafa262f9
Maximilian Meister <mmeister@suse.de>
2016-09-07 09:20:35 +0200

    adding a repository check to the node model
    to check the os and addon repositories on the nodes
    this model will be used by the upgrade api to display which repos
    are availabe/missing

--- crowbar_framework/app/models/api/node.rb
+++ crowbar_framework/app/models/api/node.rb
@@ -15,6 +15,57 @@
 #
 
 module Api
-  class Node
+  class Node < Tableless
+    def repocheck(options = {})
+      addon = options.fetch(:addon, "os")
+      features = []
+      features.push(addon)
+
+      {}.tap do |ret|
+        ret[addon] = {
+          "available" => true
+        }
+        platform = Api::Upgrade.new.target_platform(platform_exception: addon)
+
+        features.each do |feature|
+          node_architectures(addon: addon).each do |architecture|
+            unless ::Crowbar::Repository.provided_and_enabled?(feature,
+                                                               platform,
+                                                               architecture)
+              ::Openstack::Upgrade.enable_repos_for_feature(feature, Rails.logger)
+            end
+            available, repolist = ::Crowbar::Repository.provided_and_enabled_with_repolist(
+              feature, platform, architecture
+            )
+            ret[addon]["available"] &&= available
+            ret[addon]["repos"] ||= {}
+            ret[addon]["repos"].deep_merge!(repolist.deep_stringify_keys)
+          end
+        end
+      end
+    end
+
+    protected
+
+    def node_architectures(options = {})
+      addon = options.fetch(:addon, nil)
+      addon = "pacemaker" if addon == "ha"
+      architectures = []
+      proposals = Proposal.where(barclamp: addon)
+      return architectures if proposals.empty?
+
+      proposals.each do |prop|
+        next unless RoleObject.all.detect { |r| r.barclamp == addon && r.proposal? }
+        node_names = prop.properties["deployment"][addon]["elements"].values.flatten.uniq
+        next if node_names == []
+
+        nodes = node_names.map { |n| NodeObject.find_node_by_name(n) }
+        nodes.map(&:architecture).uniq.each do |arch|
+          architectures.push(arch) unless architectures.include?(arch)
+        end
+      end
+
+      architectures
+    end
   end
 end
---
+++ crowbar_framework/spec/models/api/node_spec.rb
@@ -0,0 +1,153 @@
+require "spec_helper"
+
+describe Api::Node do
+  let!(:node_repocheck) do
+    JSON.parse(
+      File.read(
+        "spec/fixtures/node_repocheck.json"
+      )
+    )
+  end
+  let!(:node_repocheck_missing) do
+    JSON.parse(
+      File.read(
+        "spec/fixtures/node_repocheck_missing.json"
+      )
+    )
+  end
+  let!(:os_repo_missing) do
+    allow(::Crowbar::Repository).to(
+      receive(:provided_and_enabled_with_repolist).with(
+        "os", "suse-12.2", "x86_64"
+      ).and_return(
+        [
+          false,
+          {
+            "missing_repos" => {
+              "x86_64" => [
+                "SLES12-SP2-Pool",
+                "SLES12-SP2-Updates"
+              ]
+            },
+            "inactive_repos" => {
+              "x86_64" => [
+                "SLES12-SP2-Pool",
+                "SLES12-SP2-Updates"
+              ]
+            }
+          }
+        ]
+      )
+    )
+  end
+  let!(:ceph_repo_missing) do
+    allow(::Crowbar::Repository).to(
+      receive(:provided_and_enabled_with_repolist).with(
+        "ceph", "suse-12.2", "x86_64"
+      ).and_return(
+        [
+          false,
+          {
+            "missing_repos" => {
+              "x86_64" => [
+                "SUSE-Enterprise-Storage-3-Pool",
+                "SUSE-Enterprise-Storage-3-Updates"
+              ]
+            },
+            "inactive_repos" => {
+              "x86_64" => [
+                "SUSE-Enterprise-Storage-3-Pool",
+                "SUSE-Enterprise-Storage-3-Updates"
+              ]
+            }
+          }
+        ]
+      )
+    )
+  end
+  let!(:ha_repo_missing) do
+    allow(::Crowbar::Repository).to(
+      receive(:provided_and_enabled_with_repolist).with(
+        "ha", "suse-12.2", "x86_64"
+      ).and_return(
+        [
+          false,
+          {
+            "missing_repos" => {
+              "x86_64" => [
+                "SLE12-SP2-HA-Pool",
+                "SLE12-SP2-HA-Updates"
+              ]
+            },
+            "inactive_repos" => {
+              "x86_64" => [
+                "SLE12-SP2-HA-Pool",
+                "SLE12-SP2-HA-Updates"
+              ]
+            }
+          }
+        ]
+      )
+    )
+  end
+
+  before(:each) do
+    allow_any_instance_of(Api::Upgrade).to(
+      receive(:target_platform).and_return("suse-12.2")
+    )
+    allow_any_instance_of(Api::Node).to(
+      receive(:node_architectures).and_return(["x86_64"])
+    )
+    allow(::Crowbar::Repository).to(
+      receive(:provided_and_enabled?).and_return(true)
+    )
+  end
+
+  context "with a successful nodes repocheck" do
+    it "finds the os repositories required to upgrade the nodes" do
+      allow(::Crowbar::Repository).to(
+        receive(:provided_and_enabled_with_repolist).with(
+          "os", "suse-12.2", "x86_64"
+        ).and_return([true, {}])
+      )
+
+      os_repo_fixture = node_repocheck.tap do |k|
+        k.delete("ceph")
+        k.delete("ha")
+      end
+
+      expect(subject.repocheck).to eq(os_repo_fixture)
+    end
+
+    it "finds the os and addon repositories required to upgrade the nodes" do
+      ["os", "ceph", "ha"].each do |feature|
+        allow(::Crowbar::Repository).to(
+          receive(:provided_and_enabled_with_repolist).with(
+            feature, "suse-12.2", "x86_64"
+          ).and_return([true, {}])
+        )
+
+        expect(subject.repocheck(addon: feature)).to eq(
+          feature.to_s => node_repocheck[feature]
+        )
+      end
+    end
+  end
+
+  context "with a failed nodes repocheck" do
+    it "doesn't find the repositories required to upgrade the nodes" do
+      expected = {}
+      got = {}
+
+      ["os", "ceph", "ha"].each do |feature|
+        # stub repolist
+        send("#{feature}_repo_missing".to_sym)
+
+        expected[feature] = node_repocheck_missing[feature]
+        got.merge!(subject.repocheck(addon: feature))
+      end
+
+      expect(got).to eq(expected)
+    end
+  end
+end
