7b10fbaa1380c32e32d308432dec4e756b0146f1
Abel Navarro <abel.navarro@gmail.com>
2016-09-09 13:21:58 +0200

    Neutron and Keystone again to be disabled

--- chef/cookbooks/crowbar/recipes/crowbar-upgrade.rb
+++ chef/cookbooks/crowbar/recipes/crowbar-upgrade.rb
@@ -47,7 +47,7 @@ when "crowbar_upgrade"
 
   bash "disable_openstack_services" do
     code <<-EOF
-      for i in $(systemctl list-units openstack* --no-legend | cut -d" " -f1 | grep -Ev 'keystone|neutron') \
+      for i in $(systemctl list-units openstack* --no-legend | cut -d" " -f1) \
                drbd.service \
                pacemaker.service;
       do
