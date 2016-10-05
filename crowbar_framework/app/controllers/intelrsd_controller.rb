313c73e32a76640ce7853f2f0cf67fae5585740e
Itxaka <igarcia@suse.com>
2016-10-05 11:20:00 +0200

    Use host and port for rest server from the environment, fallback to localhost and default port
    On node creation:
     - set the proper node+domain for the node object
     - set the node name to the short name
     - set the fqdn and domain name to the proper ones
     - set the uptime to avoid failure on node details UI
     - set the roles to just names so they are linked to the barclamps
     - set the product name, serial number so they are shown properly in the UI
     - set the disk key for removable so disks are shown properly on the UI
     - count all the memory and aggregate it to be shown on the UI
     - count the processors with an 0 index so they can be shown in the UI
     - allocate the node
     - set the node to ready status
    
    All the node creation changes also fix the node not being shown properly when going to the details
    as well as crowbar crashing when moving the node to a different group

--- crowbar_framework/app/controllers/intelrsd_controller.rb
+++ crowbar_framework/app/controllers/intelrsd_controller.rb
@@ -14,82 +14,85 @@
 # limitations under the License.
 #
 
-require 'json'
+require "json"
 
 # An important note on interfacing with Redfish APIS:
 #
-# * Only specific top level URIs may be assumed, and even these 
-#   may be absent based on the implementation. (for ex: there 
-#   might be no /redfish/v1/Systems collection on something that
-#   doesn't have compute nodes )
-# * The API will eventually be implemented on a system that breaks
-#   any data model and hence the URIs must be dynamically discovered
-# * The data model represented here using @node_object_list prepares
-#   a list of all the available Systems along with the properties 
-#   and IDs of available resources in each system. This data model
-#   needs to be appropriately mapped to the Node object of the system
-#   where the data model is employed.
-# 
+# Only specific top level URIs may be assumed, and even these
+# may be absent based on the implementation. (for ex: there
+# might be no /redfish/v1/Systems collection on something that
+# doesn't have compute nodes )
+#
+# The API will eventually be implemented on a system that breaks
+# any data model and hence the URIs must be dynamically discovered
+# The data model represented here using @node_object_list prepares
+# a list of all the available Systems along with the properties
+# and IDs of available resources in each system. This data model
+# needs to be appropriately mapped to the Node object of the system
+# where the data model is employed.
+#
 
 class IntelRSDController < ApplicationController
   attr_reader :logger, :insecure
 
-  def initialize()
-    @redfish_client = RedfishHelper::RedfishClient.new('localhost', '8443')
+  def initialize
+    host = ENV["CROWBAR_REDFISH_HOST"] || "localhost"
+    port = ENV["CROWBAR_REDFISH_PORT"] || "8443"
+    @redfish_client = RedfishHelper::RedfishClient.new(host, port)
     @node_object_list = []
   end
 
   def get_system_resource_list(sys_id, resource)
     resource_list = []
     items = @redfish_client.get_resource("Systems/#{sys_id}/#{resource}")
-    items["Members"].each do | item |
+    items["Members"].each do |item|
       item_odata_id = item["@odata.id"]
-      item_id = item_odata_id.split(/\//)[-1]
+      item_id = item_odata_id.split("/")[-1]
       resource_item = @redfish_client.get_resource("Systems/#{sys_id}/#{resource}/#{item_id}")
       resource_list.push(resource_item)
     end
-    return resource_list
+    resource_list
   end
 
   def make_node_object_for_system(sys_id)
-    nodeobject = Hash.new()
+    nodeobject = Hash.new
     nodeobject["System_Id"] = sys_id
-    ["Processors", "Memory", "MemoryChunks", 
-     "EthernetInterfaces", "Adapters"].each do | resource |
-      nodeobject["#{resource}"] = get_system_resource_list(sys_id, resource)
+    ["Processors", "Memory", "MemoryChunks",
+     "EthernetInterfaces", "Adapters"].each do |resource|
+      nodeobject[resource.to_s] = get_system_resource_list(sys_id, resource)
     end
-    return nodeobject
+    nodeobject
   end
 
-  def get_systems()
+  def get_systems
     @systems = @redfish_client.get_resource("Systems")
     sys_list = []
 
     @systems["Members"].each do |member|
       odata_id = member["@odata.id"]
-      sys_id = odata_id.split(/\//)[-1]
+      sys_id = odata_id.split("/")[-1]
       sys_list.push(sys_id)
     end
-    return sys_list
+    sys_list
   end
 
   def get_system_data(sys_id)
     system_data = @redfish_client.get_resource("Systems/#{sys_id}")
     system_object = make_node_object_for_system(sys_id)
     ["Processors", "Memory", "MemoryChunks",
-     "EthernetInterfaces", "Adapters"].each do | resource |
-      system_data["#{resource}"] = system_object["#{resource}"]
+     "EthernetInterfaces", "Adapters"].each do |resource|
+      system_data[resource.to_s] = system_object[resource.to_s]
     end
-    return system_data
+    system_data
   end
 
-  def get_rsd_nodes()
-    system_list = get_systems()
+  def get_rsd_nodes
+    system_list = get_systems
     system_list.each do |system|
       node_object = make_node_object_for_system(system)
       @node_object_list.push(node_object)
     end
-    return @node_object_list
+    @node_object_list
   end
 
   def reset_system(sys_id)
@@ -99,92 +102,98 @@ class IntelRSDController < ApplicationController
   def get_crowbar_node_object(sys_id)
     system_object = get_system_data(sys_id)
     node_name_prefix = "d"
-    node_name_prefix = "IRSD" if system_object["Oem"].has_key?("Intel_RackScale")
+    node_name_prefix = "IRSD" if system_object["Oem"].key?("Intel_RackScale")
 
     eth_interface = system_object["EthernetInterfaces"].first
-    node_name = node_name_prefix + eth_interface["MACAddress"].gsub(":", "-")
+    node_name = node_name_prefix + eth_interface["MACAddress"].tr(":", "-")
 
-    node = NodeObject.create_new node_name
-    NodeObject.initialize(node)
-    node.set['name'] = node_name 
-    node.set['target_cpu'] = ""
-    node.set['target_vendor'] = "suse"
-    node.set['host_cpu'] = ""
-    node.set['host_vendor'] = "suse"
-    node.set['kernel'] = ""   # Kernel modules and configurations
-    node.set['counters'] = "" # various network interfaces and other counters
-    node.set['hostname'] = node_name 
-    node.set['fqdn'] = node_name + "." + ChefObject.cloud_domain
-    node.set['domain'] = ChefObject.cloud_domain
+    node = NodeObject.create_new "#{node_name}.#{Crowbar::Settings.domain}".downcase
+
+    node.set["name"] = node_name
+    node.set["target_cpu"] = ""
+    node.set["target_vendor"] = "suse"
+    node.set["host_cpu"] = ""
+    node.set["host_vendor"] = "suse"
+    node.set["kernel"] = ""   # Kernel modules and configurations
+    node.set["counters"] = "" # various network interfaces and other counters
+    node.set["hostname"] = node_name
+    node.set["fqdn"] = "#{node_name}.#{Crowbar::Settings.domain}"
+    node.set["domain"] = Crowbar::Settings.domain
     ipaddress_data = eth_interface["IPv4Addresses"].first
-    node.set['ipaddress'] = ipaddress_data["Address"]
-    node.set['macaddress'] = eth_interface["MACAddress"]
+    node.set["ipaddress"] = ipaddress_data["Address"]
+    node.set["macaddress"] = eth_interface["MACAddress"]
     ip6address_data = eth_interface["IPv6Addresses"].first
-    node.set['ip6address'] = ip6address_data["Address"]
-    #node.set['ohai_time'] = ""
-    node.set['recipes'] = ""
+    node.set["ip6address"] = ip6address_data["Address"]
+    node.set["uptime"] = "1 minute"
+    node.set["recipes"] = ""
 
     # Add other roles as seen fit
-    node.set['roles'] = []
-    ["deployer-config-default", "network-config-default", "dns-config-default", 
-     "logging-config-default", "ntp-config-default", "nova-compute-kvm",
-     "provisioner-base", "provisioner-config-default", "crowbar-#{node['fqdn']}"].each do |role_name|
-      role = RoleObject.find_role_by_name "#{role_name}"
-      node['roles'] += role
-    end 
-
-    node.set['run_list'] = ["role[#{node['roles']}]"]
-    node.set['keys']['host']['host_dsa_public'] = ""
-    node.set['keys']['host']['host_rsa_public'] = ""
-    node.set['keys']['host']['host_ecdsa_public'] = ""
-    node.set['virtualization']['system'] = "kvm"
-    node.set['virtualization']['role'] = "guest"
-    node.set['platform'] = "suse"
-    node.set['platform_version'] = "12.1"
-    node.set['dmi']['bios']['all_records'] = ""
-    node.set['dmi']['bios']['vendor'] = ""
-    node.set['dmi']['bios']['version'] = system_object["BiosVersion"]
-    node.set['dmi']['bios']['release_date'] = ""
-    node.set['dmi']['bios']['address'] = ""
-    node.set['dmi']['bios']['runtime_size'] = ""
-    node.set['dmi']['bios']['rom_size'] = ""
-    node.set['dmi']['bios']['bios_revision'] = ""
-    node.set['dmi']['system']['product_name'] = ""
-    node.set['dmi']['system']['manufacturer'] = ""
-    node.set['dmi']['system']['serial_number'] = "Not Specified"
-    node.set['dmi']['system']['uuid'] = ""
-    node.set['dmi']['system']['wake_up_type'] = "Power Switch"
-    node.set['dmi']['system']['sku_number'] = "Not Specified"
-    node.set['dmi']['system']['family'] = "Not Specified"
-    node.set['dmi']['chassis']['serial_number'] = system_object['Chassis']['SerialNumber']
-    node.set['dmi']['chassis']['all_records'] = ""
-    node.set['dmi']['chassis']['manufacturer'] = ""
-    node.set['dmi']['chassis']['all_records'] = ""
-    node.set['dmi']['chassis']['boot_up_state'] = "Safe"
-    node.set['dmi']['chassis']['power_supply_state'] = "Safe"
-    node.set['block_device']['sda'] = ""
-    node.set['memory']['swap'] = ""
-    node.set['memory']['buffers'] = ""
-
-    system_object["Processors"].each do | processor |
-      id = processor["Id"]
-      node.set["cpu"]["#{id}"]['manufacturer'] = processor["Manufacturer"]
-      node.set["cpu"]["#{id}"]["model"] = processor["Model"]
-      node.set["cpu"]["#{id}"]["family"] = processor["ProcessorArchitecture"]
-      node.set["cpu"]["#{id}"]["family"] = "x86_64" if processor["InstructionSet"] == "x86-64"
-      node.set["cpu"]["#{id}"]["flags"] = processor["Capabilities"]
+    node.set["roles"] = []
+    ["deployer-config-default", "network-config-default", "dns-config-default",
+     "logging-config-default", "ntp-config-default",
+     "provisioner-base", "provisioner-config-default"].each do |role_name|
+      node["roles"] << role_name
     end
 
-    node.set['filesystem']['sysfs'] = ""
+    node.set["run_list"] = ["role[crowbar-#{node_name}.#{Crowbar::Settings.domain.tr(".", "_")}]"]
+    node.set["keys"]["host"]["host_dsa_public"] = ""
+    node.set["keys"]["host"]["host_rsa_public"] = ""
+    node.set["keys"]["host"]["host_ecdsa_public"] = ""
+    node.set["virtualization"]["system"] = "kvm"
+    node.set["virtualization"]["role"] = "guest"
+    node.set["platform"] = "suse"
+    node.set["platform_version"] = "12.1"
+    node.set["dmi"]["bios"]["all_records"] = ""
+    node.set["dmi"]["bios"]["vendor"] = ""
+    node.set["dmi"]["bios"]["version"] = system_object["BiosVersion"]
+    node.set["dmi"]["bios"]["release_date"] = ""
+    node.set["dmi"]["bios"]["address"] = ""
+    node.set["dmi"]["bios"]["runtime_size"] = ""
+    node.set["dmi"]["bios"]["rom_size"] = ""
+    node.set["dmi"]["bios"]["bios_revision"] = ""
+    node.set["dmi"]["system"]["product_name"] = system_object["Model"]
+    node.set["dmi"]["system"]["manufacturer"] = ""
+    node.set["dmi"]["system"]["serial_number"] = "Not Specified"
+    node.set["dmi"]["system"]["uuid"] = ""
+    node.set["dmi"]["system"]["wake_up_type"] = "Power Switch"
+    node.set["dmi"]["system"]["sku_number"] = "Not Specified"
+    node.set["dmi"]["system"]["family"] = "Not Specified"
+    node.set["dmi"]["chassis"]["serial_number"] = system_object["SerialNumber"]
+    node.set["dmi"]["chassis"]["all_records"] = ""
+    node.set["dmi"]["chassis"]["manufacturer"] = ""
+    node.set["dmi"]["chassis"]["all_records"] = ""
+    node.set["dmi"]["chassis"]["boot_up_state"] = "Safe"
+    node.set["dmi"]["chassis"]["power_supply_state"] = "Safe"
+    # this is needed so its counted properly for the UI
+    node.set["block_device"]["sda"] = { removable: "0" }
+    node.set["memory"]["swap"] = ""
+    node.set["memory"]["buffers"] = ""
+    total_mem = 0
+    system_object["Memory"].each do |m|
+      total_mem += m["CapacityMiB"].to_i
+    end
+    node.set["memory"]["total"] = "#{total_mem * 1024}kB"
+
+    system_object["Processors"].each do |processor|
+      id = processor["Id"].to_i - 1 # API starts at 1, we start at 0
+      node.set["cpu"][id.to_s]["manufacturer"] = processor["Manufacturer"]
+      node.set["cpu"][id.to_s]["model"] = processor["Model"]
+      node.set["cpu"][id.to_s]["family"] = processor["ProcessorArchitecture"]
+      node.set["cpu"][id.to_s]["family"] = "x86_64" if processor["InstructionSet"] == "x86-64"
+      node.set["cpu"][id.to_s]["flags"] = processor["Capabilities"]
+    end
+
+    node.set["filesystem"]["sysfs"] = ""
     node.save
+    node.allocate
+    node.set_state("ready")
   end
 end
 
-# usage of the controller APIs
-rsd_controller = IntelRSDController.new
-node_list = rsd_controller.get_rsd_nodes()
-first_node = node_list.first
-p "FIRST NODE: #{first_node}"
-# node_object = rsd_controller.get_crowbar_node_object(first_node["System_Id"])
-# p node_object
-
+# run it on a thread to not block the UI at the start
+Thread.new do
+  rsd_controller = IntelRSDController.new
+  node_list = rsd_controller.get_systems
+  first_node = node_list.first
+  rsd_controller.get_crowbar_node_object(first_node)
+end
--- crowbar_framework/app/helpers/redfish_helper.rb
+++ crowbar_framework/app/helpers/redfish_helper.rb
@@ -14,104 +14,63 @@
 # limitations under the License.
 #
 
-require 'json'
-require 'uri'
-require 'net/http'
-require 'rest-client'
+require "json"
+require "uri"
+require "net/http"
+require "rest-client"
 
 module RedfishHelper
   class RedfishClient
     attr_reader :logger
-  
+
     # Standard JSONRPC Error responses
     INVALID_JSON      = -32700
     INVALID_REQUEST   = -32600
     INVALID_PARAMS    = -32602
     METHOD_NOT_FOUND  = -32601
     INTERNAL_ERROR    = -32603
-  
+
     # RedFish-specific constants
-    REDFISH_VERSION   = "redfish/v1/"
-  
-    def initialize(host, port, insecure=true, client_cert=false)
+    REDFISH_VERSION   = "redfish/v1/".freeze
+
+    def initialize(host, port, insecure = true, client_cert = false)
       @service_uri = "https://#{host}:#{port}/#{REDFISH_VERSION}"
       @verify_ssl = OpenSSL::SSL::VERIFY_NONE if insecure
       @ssl_client_cert = false unless client_cert
     end
-  
+
     def handle_exception(json_rpc_error)
-      logger.error(json_rpc_error[:message])
+      Rails.logger.error(json_rpc_error[:message])
     end
-  
+
     def post_action(resource, action:None, params: None)
       uri = @service_uri + resource
       uri += "/Actions/#{action}" if action
-  
+
       begin
         response = RestClient::Request.execute(url: uri,
-                                             method: :post,
-                                             verify_ssl: @verify_ssl,
-                                             ssl_client_cert: @ssl_client_cert)
+                                               method: :post,
+                                               verify_ssl: @verify_ssl,
+                                               ssl_client_cert: @ssl_client_cert)
       rescue
         handle_exception(response)
       end
-      return JSON.parse(response)
+      JSON.parse(response)
     end
-  
+
     def get_resource(resource)
       uri = @service_uri + resource
-      p "QUERYING RESOURCE: #{uri}"
-  
+      Rails.logger.debug("QUERYING RESOURCE: #{uri}")
+
       begin
-        response = RestClient::Request.execute(url: uri, 
-                                         method: :get, 
-                                         verify_ssl: @verify_ssl, 
-                                         ssl_client_cert: @ssl_client_cert)
+        response = RestClient::Request.execute(url: uri,
+                                               method: :get,
+                                               verify_ssl: @verify_ssl,
+                                               ssl_client_cert: @ssl_client_cert)
       rescue
         handle_exception(response)
       end
-  
-      return JSON.parse(response)
+      JSON.parse(response)
     end
   end
 end
-
-# Usage Examples for this client library
-
-# Create a client object
-redfish_client = RedfishHelper::RedfishClient.new("localhost", "8443")
-
-# Check if the Redfish Service responds ( returns redfish/v1)
-api_resp = redfish_client.get_resource("")
-p api_resp
-
-# Check one of the API responses
-systems = redfish_client.get_resource("Systems")
-p systems
-
-sys_list = []
-
-# Loop to run through all available systems and populate a node-object
-systems["Members"].each do |member|
-  p "MEMBER DATA: #{member}"
-  member_id= member["@odata.id"]
-  p "MEMBER ID: #{member_id}"
-  sys_id = member_id.split(/\//)[-1]
-  p "SYSTEM ID: #{sys_id}"
-  sys_data = Hash.new()
-  sys_data["Systems"] = redfish_client.get_resource("Systems/#{sys_id}")
-  p "SYSTEMS DATA: #{sys_data["Systems"]}"
-  sys_data["Processors"] = redfish_client.get_resource("Systems/#{sys_id}/Processors/1")
-  p "PROCESSORS DATA: #{sys_data["Processors"]}"
-  sys_data["Memory"] = redfish_client.get_resource("Systems/#{sys_id}/Memory/1")
-  p "MEMORY DATA: #{sys_data["Memory"]}"
-  sys_data["MemoryChunks"] = redfish_client.get_resource("Systems/#{sys_id}/MemoryChunks/1")
-  p "MEMORY CHUNKS DATA: #{sys_data["MemoryChunks"]}"
-  sys_data["EthernetInterfaces"] = redfish_client.get_resource("Systems/#{sys_id}/EthernetInterfaces/1")
-  p "ETHERNET DATA: #{sys_data["EthernetInterfaces"]}"
-  sys_data["Adapters"] = redfish_client.get_resource("Systems/#{sys_id}/Adapters/1")
-  p "ADAPTERS DATA: #{sys_data["Adapters"]}"
-  sys_list.push(sys_data)
-end
-
-p "NODE OBJECT From Redfish : #{sys_list}"
