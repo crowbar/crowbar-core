5fbc0f90e238718d9e9e6cee988702c1d3fad1a7
Ralf Haferkamp <rhafer@suse.de>
2016-10-05 16:56:41 +0200

    barclamp::library: Fix test.rb for conduit resolver

--- chef/cookbooks/barclamp/libraries/test.rb
+++ chef/cookbooks/barclamp/libraries/test.rb
@@ -2,70 +2,84 @@
 if $0 == __FILE__
 
   require "minitest/autorun"
-  require_relative "conduit_resolver"
-  require_relative "barclamp_library"
+  require "conduit_resolver"
+  require "barclamp_library"
 
   class TestIFRemap < MiniTest::Test
-    def build_if_map(speeds)
-      if_map={}
-      speeds.each {|x|
-        if_map["#{x}1"]=x
+
+    def build_node_object(speeds)
+      node = {
+        "crowbar_ohai" => {
+          "detected" => {
+            "network"=> {
+            }
+          }
+        }
       }
-    if_map
+      speeds.each {|x|
+        node["crowbar_ohai"]["detected"]["network"]["#{x}"] = {
+          "speeds" => [x]
+        }
+      }
+      mock = MiniTest::Mock.new
+      # 99 is probably too much here. But node.automatic_attrs is called
+      # a lot in the conduit resolver. So we're on the save side here.
+      99.times { |_unused_| mock.expect(:automatic_attrs, node, []) }
+      mock
     end
 
     def test_match_100m
-      if_map=build_if_map %w{100m 1g}
-      b = BarclampLibrary::Barclamp::Inventory
-      assert_equal "100m", b.map_if_ref(if_map,"100m1")
+      node=build_node_object %w{100m 1g}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "100m", b.resolve_if_ref("100m1")
     end
 
     def test_match_1g
-      if_map=build_if_map %w{100m 1g}
-    b = BarclampLibrary::Barclamp::Inventory
-      assert_equal "1g", b.map_if_ref(if_map,"1g1")
+      node=build_node_object %w{100m 1g}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "1g", b.resolve_if_ref("1g1")
     end
 
     def test_10m_upgrade
-      if_map=build_if_map %w{100m 1g}
-      b = BarclampLibrary::Barclamp::Inventory
-      assert_equal "100m", b.map_if_ref(if_map,"+10m1")
+      node=build_node_object %w{100m 1g}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "100m", b.resolve_if_ref("+10m1")
     end
 
     def test_10gdowngrade
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{10m 100m 1g}
-      assert_equal "1g", b.map_if_ref(if_map,"-10g1")
+      node=build_node_object %w{10m 100m 1g}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "1g", b.resolve_if_ref("-10g1")
     end
 
     def test_1gdowngrade
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{10m 100m }
-      assert_equal "100m", b.map_if_ref(if_map,"-1g1")
+      node=build_node_object %w{10m 100m }
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "100m", b.resolve_if_ref("-1g1")
     end
 
     def test_1g_any
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{10m 100m }
-      assert_equal "100m", b.map_if_ref(if_map,"?1g1")
+      node=build_node_object %w{10m 100m }
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "100m", b.resolve_if_ref("?1g1")
     end
 
     def test_1g_any_up
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{10m 100m 10g}
-      assert_equal "10g", b.map_if_ref(if_map,"?1g1")
+      node=build_node_object %w{10m 100m 10g}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "10g", b.resolve_if_ref("?1g1")
     end
 
     def test_non_listed_items_with_one_valid
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{10m 100m 10g 0g 5k fred}
-      assert_equal "10g", b.map_if_ref(if_map,"?1g1")
+      node=build_node_object %w{10m 100m 10g 0g 5k fred}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal "10g", b.resolve_if_ref("?1g1")
     end
 
     def test_non_listed_items_with_no_valid
-      b = BarclampLibrary::Barclamp::Inventory
-      if_map=build_if_map %w{0g 5k fred}
-      assert_equal nil, b.map_if_ref(if_map,"?1g1")
+      node=build_node_object %w{0g 5k fred}
+      b = BarclampLibrary::Barclamp::NodeConduitResolver.new(node)
+      assert_equal nil, b.resolve_if_ref("?1g1")
     end
   end
 end
