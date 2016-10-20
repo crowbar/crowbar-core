4fb9c7edd8ad44657143a6f3834c5854014e6bfd
Maximilian Meister <mmeister@suse.de>
2016-10-20 16:56:07 +0200

    add a status when running upgrade prepare
    (cherry picked from commit 35d169ef772522523d47ddcde5057d9b7266e00f)

--- crowbar_framework/app/models/api/upgrade.rb
+++ crowbar_framework/app/models/api/upgrade.rb
@@ -153,6 +153,8 @@ module Api
       end
 
       def prepare(options = {})
+        ::Crowbar::UpgradeStatus.new.start_step
+
         background = options.fetch(:background, false)
 
         if background
@@ -315,9 +317,11 @@ module Api
         service_object = CrowbarService.new(Rails.logger)
         service_object.prepare_nodes_for_crowbar_upgrade
 
+        ::Crowbar::UpgradeStatus.new.end_step
         true
       rescue => e
         message = e.message
+        ::Crowbar::UpgradeStatus.new.end_step(false, { prepare_nodes_for_crowbar_upgrade: message })
         Rails.logger.error message
 
         false
