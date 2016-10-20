c862b242a8a8bd3641537ca5cac3dce48d12e040
Maximilian Meister <mmeister@suse.de>
2016-10-20 15:32:48 +0200

    determine the best upgrade mode
    none: when a required check fails
    non-disruptive: when all checks succeed
    disruptive: when only some non-required checks fail

--- crowbar_framework/spec/controllers/api/upgrade_controller_spec.rb
+++ crowbar_framework/spec/controllers/api/upgrade_controller_spec.rb
@@ -114,6 +114,9 @@ describe Api::UpgradeController, type: :request do
       allow(pacemaker).to receive(
         :ha_presence_check
       ).and_return({})
+      allow(Api::Upgrade).to receive(:checks).and_return(
+        JSON.parse(upgrade_prechecks)["checks"].deep_symbolize_keys
+      )
 
       get "/api/upgrade/prechecks", {}, headers
       expect(response).to have_http_status(:ok)
