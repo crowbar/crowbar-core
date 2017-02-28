crowbar/crowbar-core

commit 73a0ce5ff0cd335c51e0bae2175c2d8c675f97fa

chef: Use Barclamp::Inventory.Network objects
We do this instead of accessing attributes, in order to centralize the
location of the network definitions, so it can be changed.

chef/cookbooks/network/recipes/default.rb
@@ -109,8 +109,8 @@ ovs_bridge_created = false
 # Networks that use vlans and bridges will be handled later
 def net_weight(net)
   res = 0
-  if node["crowbar"]["network"][net]["use_vlan"] then res += 1 end
-  if node["crowbar"]["network"][net]["add_bridge"] then res += 1 end
+  if net.use_vlan then res += 1 end
+  if net.add_bridge then res += 1 end
   res
 end
 
@@ -142,26 +142,24 @@ def kill_nic(nic)
 end
 
 # Dynamically create our new local interfaces.
-node["crowbar"]["network"].keys.sort{|a,b|
+Barclamp::Inventory.list_networks(node).sort do |a, b|
   net_weight(a) <=> net_weight(b)
-}.each do |name|
-  next if name == "bmc"
+end.each do |network|
+  next if network.name == "bmc"
+
   net_ifs = Array.new
-  network = node["crowbar"]["network"][name]
-  next if network.empty?
-  addr = if network["address"]
-           IP.coerce("#{network["address"]}/#{network["netmask"]}")
+  addr = if network.address
+           IP.coerce("#{network.address}/#{network.netmask}")
          else
            nil
          end
-  conduit = network["conduit"]
-  base_ifs = conduit_map[conduit]["if_list"]
+  base_ifs = conduit_map[network.conduit]["if_list"]
   # Error out if we were handed an invalid conduit mapping.
   unless base_ifs.all?{ |i|i.is_a?(String) && ::Nic.exists?(i) }
-    raise ::ArgumentError.new("Conduit mapping \"#{conduit}\" for network \"#{name}\" is not sane: #{base_ifs.inspect}")
+    raise ::ArgumentError.new("Conduit mapping \"#{network.conduit}\" for network \"#{network.name}\" is not sane: #{base_ifs.inspect}")
   end
   base_ifs = base_ifs.map{ |i| ::Nic.new(i) }
-  Chef::Log.info("Using base interfaces #{base_ifs.map{ |i|i.name }.inspect} for network #{name}")
+  Chef::Log.info("Using base interfaces #{base_ifs.map{ |i| i.name }.inspect} for network #{network.name}")
   base_ifs.each do |i|
     ifs[i.name] ||= Hash.new
     ifs[i.name]["addresses"] ||= Array.new
@@ -169,20 +167,20 @@ node["crowbar"]["network"].keys.sort{|a,b|
   end
   case base_ifs.length
   when 0
-    Chef::Log.fatal("Conduit #{conduit} does not have any nics. Your config is invalid.")
+    Chef::Log.fatal("Conduit #{network.conduit} does not have any nics. Your config is invalid.")
     raise ::RangeError.new("Invalid conduit mapping #{conduit_map.inspect}")
   when 1
-    Chef::Log.info("Using interface #{base_ifs[0]} for network #{name}")
+    Chef::Log.info("Using interface #{base_ifs[0]} for network #{network.name}")
     our_iface = base_ifs[0]
   else
     # We want a bond.  Figure out what mode it should be.  Default to 5
-    team_mode = conduit_map[conduit]["team_mode"] ||
+    team_mode = conduit_map[network.conduit]["team_mode"] ||
       (node["network"]["teaming"] && node["network"]["teaming"]["mode"]) || 5
     # See if a bond that matches our specifications has already been created,
     # or if there is an empty bond lying around.
     bond = Nic::Bond.find(base_ifs)
     if bond
-      Chef::Log.info("Using bond #{bond.name} for network #{name}")
+      Chef::Log.info("Using bond #{bond.name} for network #{network.name}")
       bond.mode = team_mode if bond.mode != team_mode
     else
       existing_bond_names = Nic.nics.select{ |i| Nic::bond?(i) }.map{ |i| i.name }
@@ -190,7 +188,7 @@ node["crowbar"]["network"].keys.sort{|a,b|
       new_bond_name = (bond_names - existing_bond_names).first
 
       bond = Nic::Bond.create(new_bond_name, team_mode)
-      Chef::Log.info("Creating bond #{bond.name} for network #{name}")
+      Chef::Log.info("Creating bond #{bond.name} for network #{network.name}")
     end
     ifs[bond.name] ||= Hash.new
     ifs[bond.name]["addresses"] ||= Array.new
@@ -210,10 +208,10 @@ node["crowbar"]["network"].keys.sort{|a,b|
   net_ifs << our_iface.name
   # If we want a vlan interface, create one on top of the base physical
   # interface and/or bond that we already have
-  if network["use_vlan"]
-    vlan = "#{our_iface.name}.#{network["vlan"]}"
+  if network.use_vlan
+    vlan = "#{our_iface.name}.#{network.vlan}"
     if Nic.exists?(vlan) && Nic.vlan?(vlan)
-      Chef::Log.info("Using vlan #{vlan} for network #{name}")
+      Chef::Log.info("Using vlan #{vlan} for network #{network.name}")
       our_iface = Nic.new vlan
       have_vlan_iface = true
     else
@@ -225,12 +223,12 @@ node["crowbar"]["network"].keys.sort{|a,b|
       next unless n.kind_of?(Nic::Vlan)
       next if have_vlan_iface && n == our_iface
       next unless n.parent == our_iface.name
-      next unless n.vlan == network["vlan"].to_i
+      next unless n.vlan == network.vlan
       kill_nic(n)
     end
     unless have_vlan_iface
-      Chef::Log.info("Creating vlan #{vlan} for network #{name}")
-      our_iface = Nic::Vlan.create(our_iface,network["vlan"])
+      Chef::Log.info("Creating vlan #{vlan} for network #{network.name}")
+      our_iface = Nic::Vlan.create(our_iface, network.vlan)
     end
     ifs[our_iface.name] ||= Hash.new
     ifs[our_iface.name]["addresses"] ||= Array.new
@@ -240,17 +238,17 @@ node["crowbar"]["network"].keys.sort{|a,b|
     net_ifs << our_iface.name
   end
   # Ditto for a bridge.
-  if network["add_bridge"]
+  if network.add_bridge
     bridge = if our_iface.kind_of?(Nic::Vlan)
                "br#{our_iface.vlan}"
              else
                "br-#{name}"
              end
     br = if Nic.exists?(bridge) && Nic.bridge?(bridge)
-           Chef::Log.info("Using bridge #{bridge} for network #{name}")
+           Chef::Log.info("Using bridge #{bridge} for network #{network.name}")
            Nic.new bridge
          else
-           Chef::Log.info("Creating bridge #{bridge} for network #{name}")
+           Chef::Log.info("Creating bridge #{bridge} for network #{network.name}")
            Nic::Bridge.create(bridge)
          end
     ifs[br.name] ||= Hash.new
@@ -263,17 +261,17 @@ node["crowbar"]["network"].keys.sort{|a,b|
     our_iface = br
     net_ifs << our_iface.name
   end
-  if network["add_ovs_bridge"]
-    bridge = node[:network][:networks][name][:bridge_name] || "br-#{name}"
+  if network.add_ovs_bridge
+    bridge = network.bridge_name || "br-#{name}"
 
     # This flag is used later to enable wicked-nanny (on SUSE platforms)
     ovs_bridge_created = true
 
     br = if Nic.exists?(bridge) && Nic.ovs_bridge?(bridge)
-      Chef::Log.info("Using OVS bridge #{bridge} for network #{name}")
+      Chef::Log.info("Using OVS bridge #{bridge} for network #{network.name}")
       Nic.new bridge
     else
-      Chef::Log.info("Creating OVS bridge #{bridge} for network #{name}")
+      Chef::Log.info("Creating OVS bridge #{bridge} for network #{network.name}")
       Nic::OvsBridge.create(bridge)
     end
     ifs[br.name] ||= Hash.new
@@ -300,26 +298,26 @@ node["crowbar"]["network"].keys.sort{|a,b|
     our_iface = br
     net_ifs << our_iface.name
   end
-  if network["mtu"]
-    if ["admin", "storage", "os_sdn"].include? name
-      Chef::Log.info("Setting mtu #{network['mtu']} for #{name} network on #{our_iface.name}")
-      ifs[our_iface.name]["mtu"] = network["mtu"]
+  if network.mtu
+    if ["admin", "storage", "os_sdn"].include? network.name
+      Chef::Log.info("Setting mtu #{network.mtu} for #{network.name} network on #{our_iface.name}")
+      ifs[our_iface.name]["mtu"] = network.mtu
     else
       Chef::Log.warn("Setting mtu for #{our_iface.name} network is not supported yet, skipping")
     end
   end
   # Make sure our addresses are correct
-  if_mapping[name] = net_ifs
+  if_mapping[network.name] = net_ifs
   ifs[our_iface.name]["addresses"] ||= Array.new
   if addr
     ifs[our_iface.name]["addresses"] << addr
-    addr_mapping[name] ||= Array.new
-    addr_mapping[name] << addr.to_s
+    addr_mapping[network.name] ||= Array.new
+    addr_mapping[network.name] << addr.to_s
     # Ditto for our default route
-    if network["router_pref"] && (network["router_pref"].to_i < route_pref)
-      Chef::Log.info("#{name}: Will use #{network["router"]} as our default route")
-      route_pref = network["router_pref"].to_i
-      default_route = {nic: our_iface.name, gateway: network["router"]}
+    if network.router_pref && (network.router_pref < route_pref)
+      Chef::Log.info("#{network.name}: Will use #{network.router} as our default route")
+      route_pref = network.router_pref
+      default_route = { nic: our_iface.name, gateway: network.router }
     end
   end
 end

chef/cookbooks/nfs-server/recipes/default.rb
@@ -76,12 +76,14 @@ execute "nfs-export" do
   action :nothing
 end
 
+admin_net = Barclamp::Inventory.get_network_by_type(node, "admin")
+
 template "/etc/exports" do
   source "exports.erb"
   group "root"
   owner "root"
   mode 0644
-  variables(admin_subnet: node["network"]["networks"]["admin"]["subnet"],
-            admin_netmask: node["network"]["networks"]["admin"]["netmask"])
+  variables(admin_subnet: admin_net.subnet,
+            admin_netmask: admin_net.netmask)
   notifies :run, "execute[nfs-export]", :delayed
 end

chef/cookbooks/ntp/recipes/default.rb
@@ -23,7 +23,9 @@ else
 end
 ntp_servers = []
 servers.each do |n|
-  ntp_servers.push n[:crowbar][:network][:admin][:address] if n.name != node.name
+  next if n.name == node.name
+  n_admin_net = Barclamp::Inventory.get_network_by_type(n, "admin")
+  ntp_servers.push n_admin_net.address
 end
 if node["roles"].include?("ntp-server")
   ntp_servers += node[:ntp][:external_servers]

chef/cookbooks/barclamp/libraries/barclamp_library.rb
@@ -239,22 +239,25 @@ module BarclampLibrary
       end
 
       class Network
-        attr_reader :name, :address, :mtu, :broadcast, :netmask,
-          :subnet, :router, :vlan, :use_vlan,
-          :add_bridge, :conduit
+        attr_reader :name, :address, :broadcast, :netmask,
+          :subnet, :router, :router_pref, :mtu, :vlan, :use_vlan,
+          :add_bridge, :add_ovs_bridge, :bridge_name, :conduit
         def initialize(node, net, data)
           @node = node
           @name = net
           @address = data["address"]
-          @mtu = (data["mtu"] || 1500).to_i
           @broadcast = data["broadcast"]
           @netmask = data["netmask"]
           @subnet = data["subnet"]
           @router = data["router"]
-          @vlan = data["vlan"]
+          @router_pref = data["router_pref"].to_i
+          @mtu = (data["mtu"] || 1500).to_i
+          @vlan = data["vlan"].to_i
           @use_vlan = data["use_vlan"]
           @conduit = data["conduit"]
           @add_bridge = data["add_bridge"]
+          @add_ovs_bridge = data["add_ovs_bridge"]
+          @bridge_name = data["bridge_name"]
           # let's resolve this only if needed
           @interface = nil
           @interface_list = nil

chef/cookbooks/barclamp/libraries/nethelper.rb
@@ -11,8 +11,10 @@ class Chef
       }.select{ |a|a.kind_of? type }
     end
     def address(net="admin",type=::IP)
+      network_def = Chef::Recipe::Barclamp::Inventory.get_network_by_type(self, net)
+
       self.addresses(net,type).first ||
-        (::IP.coerce("#{self[:crowbar][:network][net][:address]}/#{self[:crowbar][:network][net][:netmask]}") rescue nil) ||
+        (::IP.coerce("#{network_def.address}/#{network_def.netmask}") unless network_def.nil?) ||
         ::IP.coerce(self[:ipaddress])
     end
     def interfaces(net="admin")

chef/cookbooks/bmc-nat/recipes/client.rb
@@ -17,21 +17,21 @@
 # It intentionally ignores the bios->enable node data flag.
 
 return if node[:platform_family] == "windows"
-nets = node[:crowbar][:network] || return
-nets[:bmc] && nets[:admin] || return
-bmc_subnet    = nets[:bmc][:subnet]
-bmc_netmask   = nets[:bmc][:netmask]
-admin_subnet  = nets[:admin][:subnet]
-admin_netmask = nets[:admin][:netmask]
+
+bmc_net = Barclamp::Inventory.get_network_by_type(node, "bmc")
+admin_net = Barclamp::Inventory.get_network_by_type(node, "admin")
+
+return if bmc_net.nil? || admin_net.nil?
+
 nat_node = search(:node, "roles:bmc-nat-router").first rescue return
 return if nat_node.nil?
-nat_address = nat_node[:crowbar][:network][:admin][:address]
+nat_admin_net = Barclamp::Inventory.get_network_by_type(nat_node, "admin")
 
-return if admin_subnet == bmc_subnet && admin_netmask == bmc_netmask
+return if admin_net.subnet == bmc_net.subnet && admin_net.netmask == bmc_net.netmask
 
-bmc_cidr = IP::IP4.netmask_to_subnet(bmc_netmask)
+bmc_cidr = IP::IP4.netmask_to_subnet(bmc_net.netmask)
 
 bash "Add route to get to our BMC via nat" do
-  code "ip route add #{bmc_subnet}/#{bmc_cidr} via #{nat_address}"
-  not_if "ip route show via #{nat_address} |grep -q #{bmc_subnet}/#{bmc_cidr}"
+  code "ip route add #{bmc_net.subnet}/#{bmc_cidr} via #{nat_admin_net.address}"
+  not_if "ip route show via #{nat_admin_net.address} | grep -q #{bmc_net.subnet}/#{bmc_cidr}"
 end

chef/cookbooks/bmc-nat/recipes/router.rb
@@ -16,22 +16,21 @@
 # Note : This script runs on both the admin and compute nodes.
 # It intentionally ignores the bios->enable node data flag.
 
-nets = node[:crowbar][:network] || return
-nets[:bmc] && nets[:admin] && nets[:bmc_vlan] || return
-bmc_subnet    = nets[:bmc][:subnet]
-bmc_netmask   = nets[:bmc][:netmask]
-admin_subnet  = nets[:admin][:subnet]
-admin_netmask = nets[:admin][:netmask]
+bmc_vlan_net = Barclamp::Inventory.get_network_by_type(node, "bmc_vlan")
+bmc_net = Barclamp::Inventory.get_network_by_type(node, "bmc")
+admin_net = Barclamp::Inventory.get_network_by_type(node, "admin")
+
+return if bmc_vlan_net.nil? || bmc_net.nil? || admin_net.nil?
 
 bash "Set up masquerading for the BMC network" do
   code <<EOC
 iptables -t nat -F POSTROUTING
-iptables -t nat -A POSTROUTING -s #{admin_subnet}/#{admin_netmask} -d #{bmc_subnet}/#{bmc_netmask} -j SNAT --to-source #{nets[:bmc_vlan][:address]}
+iptables -t nat -A POSTROUTING -s #{admin_net.subnet}/#{admin_net.netmask} -d #{bmc_net.subnet}/#{bmc_net.netmask} -j SNAT --to-source #{bmc_vlan_net.address}
 iptables -P FORWARD DROP
 iptables -F FORWARD
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -s #{admin_subnet}/#{admin_netmask} -d #{bmc_subnet}/#{bmc_netmask} -j ACCEPT
+iptables -A FORWARD -s #{admin_net.subnet}/#{admin_net.netmask} -d #{bmc_net.subnet}/#{bmc_net.netmask} -j ACCEPT
 echo 1 >/proc/sys/net/ipv4/ip_forward
 EOC
-  not_if "iptables -t nat --list -n |grep #{nets[:bmc_vlan][:address]}"
+  not_if "iptables -t nat --list -n | grep #{bmc_vlan_net.address}"
 end

chef/cookbooks/ipmi/recipes/ipmi-configure.rb
@@ -30,15 +30,19 @@ end
 bmc_user     = node[:ipmi][:bmc_user]
 bmc_password = node[:ipmi][:bmc_password]
 use_dhcp     = node[:ipmi][:use_dhcp]
-bmc_address  = node["crowbar"]["network"]["bmc"]["address"] rescue "0.0.0.0"
-bmc_netmask  = node["crowbar"]["network"]["bmc"]["netmask"] rescue "0.0.0.0"
-bmc_router   = node["crowbar"]["network"]["bmc"]["router"] rescue "0.0.0.0"
-bmc_use_vlan = node["crowbar"]["network"]["bmc"]["use_vlan"] rescue false
-bmc_vlan     = if bmc_use_vlan
-                 node["crowbar"]["network"]["bmc"]["vlan"].to_s
-               else
-                 "off"
-               end
+
+bmc_network = Barclamp::Inventory.get_network_by_type(node, "bmc")
+if bmc_network.nil?
+  bmc_address = "0.0.0.0"
+  bmc_netmask = "0.0.0.0"
+  bmc_router  = "0.0.0.0"
+  bmc_vlan    = "off"
+else
+  bmc_address = bmc_network.address
+  bmc_netmask = bmc_network.netmask
+  bmc_router  = bmc_network.router
+  bmc_vlan    = bmc_network.use_vlan ? bmc_network.vlan : "off"
+end
 
 if node["crowbar_wall"]["status"]["ipmi"]["user_set"].nil?
   node.set["crowbar_wall"]["status"]["ipmi"]["user_set"] = false

commit 3cbb907eb1a63e82c7c8a8b2d51b6956420d38b3

network: Fix undefined variable
Regression introduced in 73a0ce5ff0cd335c51e0bae2175c2d8c675f97fa

chef/cookbooks/network/recipes/default.rb
@@ -274,7 +274,7 @@ sorted_networks.each do |network|
     net_ifs << our_iface.name
   end
   if network.add_ovs_bridge
-    bridge = network.bridge_name || "br-#{name}"
+    bridge = network.bridge_name || "br-#{network.name}"
 
     # This flag is used later to enable wicked-nanny (on SUSE platforms)
     ovs_bridge_created = true