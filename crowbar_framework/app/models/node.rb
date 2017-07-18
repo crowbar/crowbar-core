e8243b8ecd5f5da4352481486cc73973e7a9b64e
Dirk Mueller <dmueller@suse.com>
2017-07-18 10:36:00 +0200

    crowbar: do a graceful shutdown/reboot when invoked via crowbar (bsc#1047636)
    Currently there is a hard reboot executed which is interrupting
    a currently pending chef-client run. Normally that shouldn't be
    a problem but in the HA cluster case it can cause significant harm.

--- crowbar_framework/app/models/node.rb
+++ crowbar_framework/app/models/node.rb
@@ -1058,7 +1058,7 @@ class Node < ChefObject
       if @node[:platform_family] == "windows"
         net_rpc_cmd(:power_cycle)
       else
-        ssh_cmd("/sbin/reboot")
+        ssh_cmd("/usr/sbin/crowbar_join --stop || /sbin/reboot")
       end
     end
     result
