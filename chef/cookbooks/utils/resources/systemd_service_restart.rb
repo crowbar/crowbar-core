030f1e4d1e8d619fe34e37b94a73bb4f2e25435f
Vincent Untz <vuntz@suse.com>
2017-09-11 10:34:42 +0200

    utils: Add utils_systemd_service_restart LWRP
    This resource adds (or removes) a systemd config snippet for a service,
    to change the behavior to Restart=on-failure for that service. This
    means systemd will automatically restart the service on failures, which
    makes a relatively good self-healing solution without pacemaker.
    
    See systemd.service(5) for details on Restart=
    
    Note that the fetch_name_from_service_resource attribute of this
    resource enables automatically fetching the systemd service name from
    the service resource, which helps remove duplication of code in the
    cookbooks when the service name is being overridden in the attributes of
    the service resource. For instance, this will work as expected:
    
      service "foobar" do
        service_name "notfoobar"
      end
    
      utils_systemd_service_restart "foobar"

---
+++ chef/cookbooks/utils/resources/systemd_service_restart.rb
@@ -0,0 +1,29 @@
+#
+# Copyright 2017, SUSE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+actions :enable, :disable
+default_action :enable
+
+attribute :service, kind_of: String, name_attribute: true
+attribute :fetch_name_from_service_resource, kind_of: [TrueClass, FalseClass], default: true
+attribute :restart,
+  kind_of: String,
+  regex: /^(no|always|on-success|on-failure|on-abnormal|on-abort|on-watchdog)$/,
+  default: "on-failure"
+attribute :restart_sec, kind_of: String
+attribute :success_exit_status, kind_of: Array
+attribute :restart_prevent_exit_status, kind_of: Array
+attribute :restart_force_exit_status, kind_of: Array
