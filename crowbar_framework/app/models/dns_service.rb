35526aedfae26918fcdd8059e0daeea8e7bd6971
Ivan Lausuch <ilausuch@suse.com>
2017-11-03 07:45:11 +0100

    DNS : skip unchanged nodes from chef run
    By using skip_batch_for_node? we can avoid running chef on nodes that do
    not change role and if there is no attribute change for the barclamp.
    
    Does not skip nodes that have changed roles, all nodes if
    attributes have changed and when deploying the barclamp for the first
    time.

--- crowbar_framework/app/models/dns_service.rb
+++ crowbar_framework/app/models/dns_service.rb
@@ -188,4 +188,21 @@ class DnsService < ServiceObject
     instance = Crowbar::DataBagConfig.instance_from_role(old_role, role)
     Crowbar::DataBagConfig.save("core", instance, @bc_name, config)
   end
+
+  # try to know if we can skip a node from running chef-client
+  def skip_unchanged_node?(node_name, old_role, new_role)
+    # if old_role is nil, then we are applying the barclamp for the first time
+    return false if old_role.nil?
+
+    # if the node changed roles, then we need to apply
+    return false if node_changed_roles?(node_name, old_role, new_role)
+
+    # if attributes have changed, we need to run
+    return false if node_changed_attributes?(node_name, old_role, new_role)
+
+    # by this point its safe to assume that we can skip the node as nothing has changed on it
+    # same attributes, same roles so skip it
+    @logger.info("#{@bc_name} skip_batch_for_node? skipping: #{node_name}")
+    true
+  end
 end
