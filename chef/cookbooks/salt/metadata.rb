e55f67e57a9db2a4363f8d640b5e11b694f55078
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-01-02 13:33:11 +0100

    [salt] add basic files

---
+++ chef/cookbooks/salt/metadata.rb
@@ -0,0 +1,24 @@
+#
+# Copyright 2018, SUSE LINUX GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+name "salt"
+maintainer "Crowbar Project"
+maintainer_email "bwiedemann@suse de"
+license "Apache 2.0"
+description "salt setup"
+long_description "setup of salt management for nodes"
+version "0.1"
+
+# depends "utils"
