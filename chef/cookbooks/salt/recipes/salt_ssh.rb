60d78297b3c13c92dc0e1e11cf19ec5a5f4e9cc4
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-01-02 21:00:55 +0100

    [salt] create roster file
    for salt-ssh to locate nodes to be managed
    and symlink required ssh keys because those are already deployed on nodes
    so that we do not have to manage our own set of keys
    
    (cherry picked from commit 5dd59a85e7c7314216bbfcb8bdf29447cc03e796)

--- chef/cookbooks/salt/recipes/salt_ssh.rb
+++ chef/cookbooks/salt/recipes/salt_ssh.rb
@@ -4,3 +4,43 @@
 package "salt-ssh" do
   action :install
 end
+
+nodes = node_search_with_cache("roles:network")
+roster = []
+nodes.each do |n|
+  next if n[:dns].nil?
+  base_name_no_net = n[:fqdn].chomp(".#{n[:dns][:domain]}")
+  nalias = n["crowbar"]["display"]["alias"] rescue nil
+  nalias = base_name_no_net unless nalias && !nalias.empty?
+  node_admin_ip = Barclamp::Inventory.get_network_by_type(n, "admin").address
+  roster.push(
+    fqdn: n[:fqdn],
+    ip: node_admin_ip,
+    alias: nalias
+  )
+end
+roster.sort_by! { |n| n[:alias] }
+
+# Rewrite our roster file
+template "/etc/salt/roster" do
+  source "roster.erb"
+  mode 0o644
+  owner "root"
+  group "root"
+  variables(roster: roster)
+end
+
+directory "/etc/salt/pki/master/ssh" do
+  owner "root"
+  group "root"
+  mode 0o755
+  action :create
+end
+
+link "/etc/salt/pki/master/ssh/salt-ssh.rsa" do
+  to "/root/.ssh/id_rsa"
+end
+
+link "/etc/salt/pki/master/ssh/salt-ssh.rsa.pub" do
+  to "/root/.ssh/id_rsa.pub"
+end
