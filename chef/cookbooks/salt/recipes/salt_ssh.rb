45f6b2931da62850ebe5e319a9f5e22485441127
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-01-02 13:33:11 +0100

    [salt] add basic files
    (cherry picked from commit e55f67e57a9db2a4363f8d640b5e11b694f55078)

---
+++ chef/cookbooks/salt/recipes/salt_ssh.rb
@@ -0,0 +1,6 @@
+# requires repos
+# http://download.nue.suse.com/ibs/SUSE/Products/SLE-Module-Adv-Systems-Management/12/x86_64/product/
+# http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Adv-Systems-Management/12/x86_64/update/
+package "salt-ssh" do
+  action :install
+end
