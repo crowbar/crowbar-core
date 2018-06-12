crowbar/crowbar-core

commit cdb5be7e52ca5895533aed7c47671123dad543a0

upgrade: Finalize upgrade of controller nodes after they are done
Once all controller nodes are upgraded, we can prepare them for
regular crowbar usage. Finalizing removes rest of upgrade related
variables so the nodes are not treated as being upgraded.

crowbar_framework/app/models/api/upgrade.rb
@@ -746,6 +746,10 @@ module Api
         when "controllers"
           # Upgrade controller clusters only
           do_controllers_substep(substep)
+          # Finalize only upgraded nodes (compute nodes might be postponed)
+          ::Node.find("state:crowbar_upgrade").each do |node|
+            finalize_node_upgrade node
+          end
         else
           # Upgrade given compute node
           upgrade_one_compute_node component

commit 836f95dc460c07ced53926992a7be62e18bbcad2

upgrade: Do not finalize nodes that are not upgraded
Mistakingly, we were finalizing the nodes that were still not upgraded
which left to deletion of upgrade related scripts on those nodes.

It was broken with commit cdb5be7e52ca5895533aed7c47671123dad543a0

crowbar_framework/app/models/api/upgrade.rb
@@ -728,7 +728,7 @@ module Api
           # Upgrade controller clusters only
           do_controllers_substep(substep)
           # Finalize only upgraded nodes (compute nodes might be postponed)
-          ::Node.find("state:crowbar_upgrade").each do |node|
+          ::Node.find("state:ready").each do |node|
             finalize_node_upgrade node
           end
         else