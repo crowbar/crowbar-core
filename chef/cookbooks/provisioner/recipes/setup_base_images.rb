0aa525fd8bd141bb4d848d8b52fb89adb6eea772
Vincent Untz <vuntz@suse.com>
2019-04-02 11:13:46 +0200

    provisioner: Use restricted client for crowbar_register (bsc#1117080)
    Avoid using the machine-install user that is privileged, and do not
    create /etc/crowbar.install.key with the credentials anymore: the
    restricted client user is all that is needed nowadays.
    
    This fixes the first part of bsc#1117080, SCRD-7537, CVE-2018-17954.
    
    (cherry picked from commit 59cd5f6032250338c98a393f98f0d1fb4bfc7cc9)
    (cherry picked from commit 09ab4496d976fcd4d1d61f0c7ce5e91dfddd9b10)

--- chef/cookbooks/provisioner/recipes/setup_base_images.rb
+++ chef/cookbooks/provisioner/recipes/setup_base_images.rb
@@ -29,6 +29,8 @@ enable_pxe = node[:provisioner][:enable_pxe]
 crowbar_node = node_search_with_cache("roles:crowbar").first
 crowbar_protocol = crowbar_node[:crowbar][:apache][:ssl] ? "https" : "http"
 crowbar_verify_ssl = !crowbar_node["crowbar"]["apache"]["insecure"]
+crowbar_client_username = crowbar_node["crowbar"]["client_user"]["username"]
+crowbar_client_password = crowbar_node["crowbar"]["client_user"]["password"]
 
 tftproot = node[:provisioner][:root]
 
@@ -535,11 +537,12 @@ node[:provisioner][:supported_oses].each do |os, arches|
                   crowbar_protocol: crowbar_protocol,
                   crowbar_verify_ssl: crowbar_verify_ssl,
                   enable_pxe: enable_pxe,
+                  crowbar_client_username: crowbar_client_username,
+                  crowbar_client_password: crowbar_client_password,
                   web_port: web_port,
                   ntp_servers_ips: ntp_servers,
                   os: os,
                   arch: arch,
-                  crowbar_key: crowbar_key,
                   domain: domain_name,
                   repos: repos,
                   is_ses: storage_available && !cloud_available,
