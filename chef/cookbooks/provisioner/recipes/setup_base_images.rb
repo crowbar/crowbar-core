62f6f38700908b3cdbd435ea89b5e41e268a6c1e
Bogdano Arendartchuk <barendartchuk@suse.com>
2019-05-14 12:06:20 +0200

    deployer: Use dhcp on crowbar_register only when enable_pxe is set (bsc#1132654)
    (cherry picked from commit 80db03a430713debf395b676564f4446aaf6fc50)
    (cherry picked from commit c63d8d5d0cb29a649d0371f194446b1cfd240f7c)

--- chef/cookbooks/provisioner/recipes/setup_base_images.rb
+++ chef/cookbooks/provisioner/recipes/setup_base_images.rb
@@ -24,6 +24,7 @@ domain_name = node[:dns].nil? ? node[:domain] : (node[:dns][:domain] || node[:do
 web_port = node[:provisioner][:web_port]
 provisioner_web="http://#{admin_ip}:#{web_port}"
 append_line = node[:provisioner][:discovery][:append].dup # We'll modify it inline
+enable_pxe = node[:provisioner][:enable_pxe]
 
 crowbar_node = node_search_with_cache("roles:crowbar").first
 crowbar_protocol = crowbar_node[:crowbar][:apache][:ssl] ? "https" : "http"
@@ -533,6 +534,7 @@ node[:provisioner][:supported_oses].each do |os, arches|
                   admin_broadcast: admin_net.broadcast,
                   crowbar_protocol: crowbar_protocol,
                   crowbar_verify_ssl: crowbar_verify_ssl,
+                  enable_pxe: enable_pxe,
                   web_port: web_port,
                   ntp_servers_ips: ntp_servers,
                   os: os,
