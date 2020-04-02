47adbb18c4d174940b188ec8e7d8c1f4e29a1174
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2020-04-03 00:58:53 +0200

    provisioner: Fix ssh key validation (SOC-11126)
    Fixed ssh keys validation wrt (optional) options field before the
    type/method in key line.
    Additional improvement is proper handling of comments with spaces and
    extended list of supported key types.
    
    (cherry picked from commit a19750f825c33fa31aced174cb4fe1afba99a1b4)

--- chef/cookbooks/barclamp/libraries/barclamp_library.rb
+++ chef/cookbooks/barclamp/libraries/barclamp_library.rb
@@ -14,6 +14,7 @@
 #
 
 require_relative "conduit_resolver.rb"
+require_relative "ssh_key_parser.rb"
 
 module BarclampLibrary
   class Barclamp
@@ -473,6 +474,10 @@ module BarclampLibrary
           @cache["groups"][group].fetch(instance, {}).fetch(barclamp, {})
         end
       end
+    end
+
+    class SSHKeyParser
+      include Crowbar::SSHKeyParser
     end
   end
 end
